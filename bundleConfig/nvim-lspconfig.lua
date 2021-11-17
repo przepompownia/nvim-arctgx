08521a1b2f2cc5253956e3d9978977b255f064fe
przepompownia <przepompownia@users.noreply.github.com>
2021-11-17 18:35:54 +0100

    lspconfig: configure sumneko_lua

--- bundleConfig/nvim-lspconfig.lua
+++ bundleConfig/nvim-lspconfig.lua
@@ -219,6 +219,36 @@ require'lspconfig'.diagnosticls.setup{
   }
 }
 
+-- todo check if path exist
+local sumneko_root_path = os.getenv( "HOME" )..'/dev/external/lua-language-server'
+local sumneko_binary = sumneko_root_path.."/bin/Linux/lua-language-server"
+
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+
+require'lspconfig'.sumneko_lua.setup {
+  cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+  capabilities = capabilities,
+  on_attach = on_attach,
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+        path = runtime_path,
+      },
+      diagnostics = {
+        globals = {'vim'},
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
 
 local servers = { 'bashls', 'phpactor', 'vimls', 'dockerls' }
 for _, lsp in ipairs(servers) do
