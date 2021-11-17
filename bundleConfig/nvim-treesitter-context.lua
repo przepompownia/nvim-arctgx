ef99e5506a16dcf13158113477c8a36545bccf31
przepompownia <przepompownia@users.noreply.github.com>
2021-11-17 19:05:14 +0100

    Configure nvim-treesitter-context

---
+++ bundleConfig/nvim-treesitter-context.lua
@@ -0,0 +1,22 @@
+require'treesitter-context'.setup{
+    enable = true,
+    throttle = true,
+    max_lines = 0,
+    patterns = {
+        default = {
+            'class',
+            'function',
+            'method',
+            -- 'for', -- These won't appear in the context
+            -- 'while',
+            -- 'if',
+            -- 'switch',
+            -- 'case',
+        },
+        -- Example for a specific filetype.
+        -- If a pattern is missing, *open a PR* so everyone can benefit.
+        --   rust = {
+        --       'impl_item',
+        --   },
+    },
+}
