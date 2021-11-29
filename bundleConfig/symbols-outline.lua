532c4d00ad01a998837686150771773d005f1e61
przepompownia <przepompownia@users.noreply.github.com>
2021-11-29 19:37:01 +0100

    Configure symbols-outline

--- bundleConfig/symbols-outline.lua
+++ bundleConfig/symbols-outline.lua
@@ -1,3 +1,12 @@
+vim.g.symbols_outline = {
+  width = 50,  
+  auto_preview = false,
+  symbol_blacklist = {
+    'Variable',
+  },    
+}
+
 vim.api.nvim_exec([[
   nmap <Plug>(ide-outline) <Cmd>SymbolsOutline<CR>
-]], false)
+  hi FocusedSymbol gui=italic guibg=#dddddd
+  ]], false)
