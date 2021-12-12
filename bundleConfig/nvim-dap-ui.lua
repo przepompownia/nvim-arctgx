cc3e2abe9a1ce84dcc50751ffb71cf713013a1df
przepompownia <przepompownia@users.noreply.github.com>
2021-12-12 21:54:03 +0100

    Further nvim-dap mapping completion

--- bundleConfig/nvim-dap-ui.lua
+++ bundleConfig/nvim-dap-ui.lua
@@ -1,4 +1,5 @@
 require('dapui').setup({})
 vim.cmd([[
     nnoremap <silent> <Plug>(ide-debugger-ui-toggle) :lua require'dapui'.toggle()<CR>
+    xmap <Plug>(ide-debugger-eval-popup) <Cmd>lua require("dapui").eval()<CR>
 ]])
---
+++ bundleConfig/nvim-dap.lua
@@ -0,0 +1,22 @@
+local dap = require 'dap'
+
+dap.adapters.php = {
+  type = 'executable',
+  command = 'node',
+  args = { '/home/arctgx/.vim/pack/bundle/opt/arctgx/bundleConfig/vimspector-config/gadgets/linux/vscode-php-debug/out/phpDebug.js' }
+}
+
+vim.cmd([[
+    nnoremap <silent> <Plug>(ide-debugger-run) :lua require'dap'.continue()<CR>
+    nnoremap <silent> <Plug>(ide-debugger-step-over) :lua require'dap'.step_over()<CR>
+    nnoremap <silent> <Plug>(ide-debugger-step-into) :lua require'dap'.step_into()<CR>
+    nnoremap <silent> <Plug>(ide-debugger-step-out) :lua require'dap'.step_out()<CR>
+    nnoremap <silent> <Plug>(ide-debugger-toggle-breakpoint) :lua require'dap'.toggle_breakpoint()<CR>
+    nnoremap <silent> <Plug>(ide-debugger-toggle-breakpoint-conditional) :lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>
+    nnoremap <silent> <Plug>(ide-debugger-up-frame) :lua require'dap'.up()<CR>
+    nnoremap <silent> <Plug>(ide-debugger-down-frame) :lua require'dap'.down()<CR>
+    nnoremap <silent> <Plug>(ide-debugger-run-to-cursor) :lua require'dap'.run_to_cursor()<CR>
+]])
+-- nnoremap <silent> <leader>lp :lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>
+-- nnoremap <silent> <leader>dr :lua require'dap'.repl.open()<CR>
+-- nnoremap <silent> <leader>dl :lua require'dap'.run_last()<CR>
