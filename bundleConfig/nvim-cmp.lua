27cf71a411336af5021561ed43ceac91626d2222
przepompownia <przepompownia@users.noreply.github.com>
2021-12-24 02:37:18 +0100

    cmp: add luasnip source

--- bundleConfig/nvim-cmp.lua
+++ bundleConfig/nvim-cmp.lua
@@ -4,7 +4,7 @@ local cmp_buffer = require('cmp_buffer')
 cmp.setup({
   snippet = {
     expand = function(args)
-      vim.fn["UltiSnips#Anon"](args.body)
+      require('luasnip').lsp_expand(args.body)
     end,
   },
   mapping = {
@@ -22,7 +22,7 @@ cmp.setup({
   },
   sources = {
     { name = 'nvim_lsp' },
-    { name = 'ultisnips' },
+    { name = 'luasnip' },
     { name = 'buffer' },
   },
   sorting = {
