22e09fc636e45a9db7c70358599fc518fbeebc60
przepompownia <przepompownia@users.noreply.github.com>
2021-12-27 00:39:50 +0100

    Telescope: add mapping to toggle preview

--- bundleConfig/telescope.nvim.lua
+++ bundleConfig/telescope.nvim.lua
@@ -1,6 +1,7 @@
 local actions = require 'telescope.actions'
 local transform_mod = require('telescope.actions.mt').transform_mod
 local action_set = require 'telescope.actions.set'
+local action_layout = require 'telescope.actions.layout'
 
 local customActions = transform_mod({
   tabDrop = function(prompt_bufnr)
@@ -16,6 +17,7 @@ require('telescope').setup {
         ['<C-n>'] = actions.cycle_history_prev,
         ['<A-Up>'] = actions.preview_scrolling_up,
         ['<A-Down>'] = actions.preview_scrolling_down,
+        ['<A-/>'] = action_layout.toggle_preview,
         ['<CR>'] = customActions.tabDrop,
       },
     },
