64310ae840a73240121d1853153b5df20a4162ce
przepompownia <przepompownia@users.noreply.github.com>
2022-01-07 14:53:36 +0100

    LSP mappings: use lua functions directly

--- lua/arctgx/lsp.lua
+++ lua/arctgx/lsp.lua
@@ -1,37 +1,54 @@
+local api = vim.api
+local lsp = require('vim.lsp')
+local diagnostic = require('vim.diagnostic')
+
 local M = {
 }
 
+local common_opts = function(fn)
+  return { callback=fn, noremap=true, silent=true }
+end
+
+local print_workspace_folders = function ()
+  print(vim.inspect(lsp.buf.list_workspace_folders()))
+end
+
 function M.on_attach(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+  local function buf_map(mode, lhs, lua_rhs, opts)
+    opts = opts or {}
+    opts.callback = lua_rhs
+    opts.noremap = opts.noremap or true
+    opts.silent = opts.silent or true
+    api.nvim_buf_set_keymap(bufnr, mode, lhs, '', opts)
+  end
+
+  local function buf_set_option(...) api.nvim_buf_set_option(bufnr, ...) end
 
   buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  local opts = { noremap=true, silent=true }
-
-  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', '<Plug>(ide-goto-definition)', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', '<Plug>(ide-hover)', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', '<Plug>(ide-goto-implementation)', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', '<Plug>(ide-show-signature-help)', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('i', '<Plug>(ide-show-signature-help)', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  buf_set_keymap('n', '<Plug>(ide-list-workspace-symbols)', '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>', opts)
-  buf_set_keymap('n', '<Plug>(ide-list-document-symbols)', '<cmd>lua vim.lsp.buf.document_symbol()<CR>', opts)
-  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', '<Plug>(ide-action-rename)', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  buf_set_keymap('n', '<Plug>(ide-find-references)', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', '<Plug>(ide-diagnostic-info)', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-  buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
-  buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  buf_map('n', '<Plug>(ide-goto-definition)', lsp.buf.definition)
+  buf_map('n', '<Plug>(ide-hover)', lsp.buf.hover)
+  buf_map('n', '<Plug>(ide-goto-implementation)', lsp.buf.implementation)
+  buf_map('n', '<Plug>(ide-show-signature-help)', lsp.buf.signature_help)
+  buf_map('i', '<Plug>(ide-show-signature-help)', lsp.buf.signature_help)
+  buf_map('n', '<Plug>(ide-list-workspace-symbols)', lsp.buf.workspace_symbol)
+  buf_map('n', '<Plug>(ide-list-document-symbols)', lsp.buf.document_symbol)
+  buf_map('n', '<Plug>(ide-action-rename)', lsp.buf.rename)
+  buf_map('n', '<Plug>(ide-find-references)', lsp.buf.references)
+  buf_map('n', '<Plug>(ide-diagnostic-info)', diagnostic.open_float)
+  buf_map('n', 'gD', lsp.buf.declaration)
+  buf_map('n', '<space>wa', lsp.buf.add_workspace_folder)
+  buf_map('n', '<space>wr', lsp.buf.remove_workspace_folder)
+  buf_map('n', '<space>wl', print_workspace_folders)
+  buf_map('n', '<space>D', lsp.buf.type_definition)
+  buf_map('n', '<space>ca', lsp.buf.code_action)
+  buf_map('n', '[d', diagnostic.goto_prev)
+  buf_map('n', ']d', diagnostic.goto_next)
+  buf_map('n', '<space>q', diagnostic.setloclist)
+  buf_map('n', '<space>f', lsp.buf.formatting)
 
   if client.resolved_capabilities.document_highlight then
-    vim.api.nvim_exec([[
+    api.nvim_exec([[
       hi LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
       hi LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
       hi LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
