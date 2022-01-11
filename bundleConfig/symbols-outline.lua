a65690750e0f3bb7c40b38441c3e2459092b52c0
przepompownia <przepompownia@users.noreply.github.com>
2022-01-11 01:33:57 +0100

    symbols-outline: use keymap

--- bundleConfig/symbols-outline.lua
+++ bundleConfig/symbols-outline.lua
@@ -1,15 +1,19 @@
+local keymap = require('vim.keymap')
+local so = require('symbols-outline')
+
 vim.g.symbols_outline = {
-  width = 50,  
+  width = 50,
   auto_preview = false,
   symbol_blacklist = {
     'Variable',
-  },    
+  },
   keymaps = {
     goto_location = {'<CR>', '<2-LeftMouse>'},
   },
 }
 
+keymap.set({'n'}, '<Plug>(ide-outline)', so.toggle_outline)
+
 vim.api.nvim_exec([[
-  nmap <Plug>(ide-outline) <Cmd>SymbolsOutline<CR>
   hi FocusedSymbol gui=italic guibg=#dddddd
   ]], false)
