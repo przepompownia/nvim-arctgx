85e47dfd0c8faaa35e471cff2660066215d25cb2
przepompownia <przepompownia@users.noreply.github.com>
2022-01-11 02:03:36 +0100

    Confiure litee.nvim

---
+++ bundleConfig/litee.nvim.lua
@@ -0,0 +1,11 @@
+require('litee.lib').setup({
+  panel = {
+    orientation = 'right',
+    panel_size = 50,
+  },
+  tree = {
+    icon_set = 'nerd',
+    indent_guides = false,
+    expanded = false,
+  },
+})
