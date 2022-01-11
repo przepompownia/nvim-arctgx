dc6a187955fbd78d922c1716a9d5e52052683bdb
przepompownia <przepompownia@users.noreply.github.com>
2022-01-11 01:43:39 +0100

    litee-symboltree: enable by ide-outline

--- bundleConfig/litee-symboltree.nvim.lua
+++ bundleConfig/litee-symboltree.nvim.lua
@@ -1,4 +1,7 @@
+local keymap = require('vim.keymap')
 require('litee.symboltree').setup({
   on_open = 'panel',
   map_resize_keys = false,
 })
+
+keymap.set({'n'}, '<Plug>(ide-outline)', vim.lsp.buf.document_symbol)
