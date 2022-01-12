cbde6fad0dedf103b254efc2652b78b8428b677b
przepompownia <przepompownia@users.noreply.github.com>
2022-01-12 23:06:52 +0100

    litee-symboltree: configure keymaps

--- bundleConfig/litee-symboltree.nvim.lua
+++ bundleConfig/litee-symboltree.nvim.lua
@@ -2,6 +2,21 @@ local keymap = require('vim.keymap')
 require('litee.symboltree').setup({
   on_open = 'panel',
   map_resize_keys = false,
+  keymaps = {
+    hide = '<Esc>',
+    expand = '<Right>',
+    collapse = '<Left>',
+    collapse_all = 'zM',
+    jump = '<CR>',
+    jump_split = 's',
+    jump_vsplit = 'v',
+    jump_tab = 't',
+    hover = 'i',
+    details = 'd',
+    close = 'X',
+    close_panel_pop_out = 'H',
+    help = '?',
+  },
 })
 
 keymap.set({'n'}, '<Plug>(ide-outline)', vim.lsp.buf.document_symbol)
