dcd1277a8abf4247f44c6b0e1b9831de8bc8acbd
przepompownia <przepompownia@users.noreply.github.com>
2022-01-15 16:41:15 +0100

    nvim-dap: use lua keymap

--- bundleConfig/nvim-dap.lua
+++ bundleConfig/nvim-dap.lua
@@ -1,4 +1,6 @@
 local dap = require 'dap'
+local keymap = require 'vim.keymap'
+local vim = vim
 
 dap.adapters.php = {
   type = 'executable',
@@ -12,17 +14,21 @@ vim.fn.sign_define('DapBreakpointRejected', {text='R', texthl='IdeCodeWindowCurr
 vim.fn.sign_define('DapLogPoint', {text='L', texthl='IdeCodeWindowCurrentFrameSign', linehl='', numhl='IdeBreakpointLineNr'})
 vim.fn.sign_define('DapStopped', {text='▶', texthl='IdeCodeWindowCurrentFrameSign', linehl='CursorLine', numhl=''})
 
-vim.cmd([[
-    nnoremap <silent> <Plug>(ide-debugger-run) :lua require'dap'.continue()<CR>
-    nnoremap <silent> <Plug>(ide-debugger-step-over) :lua require'dap'.step_over()<CR>
-    nnoremap <silent> <Plug>(ide-debugger-step-into) :lua require'dap'.step_into()<CR>
-    nnoremap <silent> <Plug>(ide-debugger-step-out) :lua require'dap'.step_out()<CR>
-    nnoremap <silent> <Plug>(ide-debugger-toggle-breakpoint) :lua require'dap'.toggle_breakpoint()<CR>
-    nnoremap <silent> <Plug>(ide-debugger-toggle-breakpoint-conditional) :lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>
-    nnoremap <silent> <Plug>(ide-debugger-up-frame) :lua require'dap'.up()<CR>
-    nnoremap <silent> <Plug>(ide-debugger-down-frame) :lua require'dap'.down()<CR>
-    nnoremap <silent> <Plug>(ide-debugger-run-to-cursor) :lua require'dap'.run_to_cursor()<CR>
-]])
+local opts = {silent = true, noremap = true}
+keymap.set({'n'}, '<Plug>(ide-debugger-run)', dap.continue, opts)
+keymap.set({'n'}, '<Plug>(ide-debugger-step-over)', dap.step_over, opts)
+keymap.set({'n'}, '<Plug>(ide-debugger-step-into)', dap.step_into, opts)
+keymap.set({'n'}, '<Plug>(ide-debugger-step-out)', dap.step_out, opts)
+keymap.set({'n'}, '<Plug>(ide-debugger-toggle-breakpoint)', dap.toggle_breakpoint, opts)
+keymap.set(
+  {'n'},
+  '<Plug>(ide-debugger-toggle-breakpoint-conditional)',
+  function() dap.set_breakpoint(vim.fn.input('Breakpoint condition: ')) end,
+  opts
+)
+keymap.set({'n'}, '<Plug>(ide-debugger-up-frame)', dap.up, opts)
+keymap.set({'n'}, '<Plug>(ide-debugger-down-frame)', dap.down, opts)
+keymap.set({'n'}, '<Plug>(ide-debugger-run-to-cursor)', dap.run_to_cursor, opts)
 -- nnoremap <silent> <leader>lp :lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>
 -- nnoremap <silent> <leader>dr :lua require'dap'.repl.open()<CR>
 -- nnoremap <silent> <leader>dl :lua require'dap'.run_last()<CR>
