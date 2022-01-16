059e6f7e74d9077971c53efe337a1ba71c0db8a5
przepompownia <przepompownia@users.noreply.github.com>
2022-01-16 21:38:05 +0100

    lsp-config: location_handler apply latest changes from neovim

--- plugin/lsp-config.lua
+++ plugin/lsp-config.lua
@@ -5,42 +5,21 @@ local api = vim.api
 local log = require 'vim.lsp.log'
 local util = require 'vim.lsp.util'
 
-local function get_line_byte_from_position(bufnr, position)
-  -- LSP's line and characters are 0-indexed
-  -- Vim's line and columns are 1-indexed
-  local col = position.character
-  -- When on the first character, we can ignore the difference between byte and
-  -- character
-  if col > 0 then
-    local line = position.line
-    local lines = api.nvim_buf_get_lines(bufnr, line, line + 1, false)
-    if #lines > 0 then
-      return vim.str_byteindex(lines[1], col)
-    end
-  end
-  return col
-end
-
 -- based on lsp/util.lua
-local tab_drop_location = function(location)
+local tab_drop_location = function(location, offset_encoding)
   -- location may be Location or LocationLink
   local uri = location.uri or location.targetUri
   if uri == nil then return end
-  -- Save position in jumplist
-  vim.cmd "normal! m'"
+  if offset_encoding == nil then
+    vim.notify_once('jump_to_location must be called with valid offset encoding', vim.log.levels.WARN)
+  end
 
-  -- Push a new item into tagstack
-  -- local from = {vim.fn.bufnr('%'), vim.fn.line('.'), vim.fn.col('.'), 0}
-  -- local items = {{tagname=vim.fn.expand('<cword>'), from=from}}
-  -- vim.fn.settagstack(vim.fn.win_getid(), {items=items}, 't')
-
-  --- Jump to new location (adjusting for UTF-16 encoding of characters)
   local range = location.range or location.targetSelectionRange
   local row = range.start.line
   local bufnr = vim.uri_to_bufnr(uri)
   local path = vim.api.nvim_buf_get_name(bufnr)
 
-  local col = get_line_byte_from_position(bufnr, range.start)
+  local col = util._get_line_byte_from_position(bufnr, range.start, offset_encoding)
   base.tab_drop(path, row + 1, col + 1)
 
   vim.cmd 'normal zt'
@@ -54,23 +33,27 @@ local function location_handler(_, result, ctx, _)
 
     return nil
   end
+  local client = vim.lsp.get_client_by_id(ctx.client_id)
 
   -- textDocument/definition can return Location or Location[]
   -- https://microsoft.github.io/language-server-protocol/specifications/specification-current/#textDocument_definition
 
   if not vim.tbl_islist(result) then
-    tab_drop_location(result)
+    tab_drop_location(result, client.offset_encoding)
 
     return
   end
 
   if #result == 1 then
-    tab_drop_location(result[1])
+    tab_drop_location(result[1], client.offset_encoding)
 
     return
   end
 
-  vim.fn.setqflist({}, ' ', {title = 'LSP locations', items = util.locations_to_items(result)})
+  vim.fn.setqflist({}, ' ', {
+    title = 'LSP locations',
+    items = util.locations_to_items(result, client.offset_encoding),
+  })
   api.nvim_command('copen')
 end
 
