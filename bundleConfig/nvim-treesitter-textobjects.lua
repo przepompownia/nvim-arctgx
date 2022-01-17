d2bf83920ecc6caae0a39e4ab304d5bf627558e3
przepompownia <przepompownia@users.noreply.github.com>
2022-01-18 00:04:30 +0100

    Add initial nvim-treesitter-textobjects config

---
+++ bundleConfig/nvim-treesitter-textobjects.lua
@@ -0,0 +1,60 @@
+require'nvim-treesitter.configs'.setup {
+  textobjects = {
+    select = {
+      enable = true,
+
+      lookahead = true,
+
+      keymaps = {
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['ac'] = '@class.outer',
+        ['ic'] = '@class.inner',
+
+        ['iF'] = {
+          python = '(function_definition) @function',
+          cpp = '(function_definition) @function',
+          c = '(function_definition) @function',
+          java = '(method_declaration) @function',
+        },
+      },
+    },
+    move = {
+      enable = true,
+      set_jumps = true,
+      goto_next_start = {
+        [']m'] = '@function.outer',
+        [']]'] = '@class.outer',
+      },
+      goto_next_end = {
+        [']M'] = '@function.outer',
+        [']['] = '@class.outer',
+      },
+      goto_previous_start = {
+        ['[m'] = '@function.outer',
+        ['[['] = '@class.outer',
+      },
+      goto_previous_end = {
+        ['[M'] = '@function.outer',
+        ['[]'] = '@class.outer',
+      },
+    },
+    swap = {
+      enable = true,
+      swap_next = {
+        ['<leader>a'] = '@parameter.inner',
+      },
+      swap_previous = {
+        ['<leader>A'] = '@parameter.inner',
+      },
+    },
+    lsp_interop = {
+      enable = true,
+      border = 'none',
+      peek_definition_code = {
+        ['<leader>df'] = '@function.outer',
+        ['<leader>dF'] = '@class.outer',
+      },
+    },
+  },
+}
