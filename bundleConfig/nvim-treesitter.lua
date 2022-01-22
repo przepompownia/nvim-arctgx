810caf00b247ef35935722077eac0d1a20b9c7c5
przepompownia <przepompownia@users.noreply.github.com>
2022-01-22 14:49:16 +0100

    nvim-treesitter: use vim.opt

--- bundleConfig/nvim-treesitter.lua
+++ bundleConfig/nvim-treesitter.lua
@@ -32,7 +32,5 @@ require'nvim-treesitter.configs'.setup {
   },
 }
 
-vim.cmd([[
-  set foldmethod=expr
-  set foldexpr=nvim_treesitter#foldexpr()
-]])
+vim.opt.foldmethod='expr'
+vim.opt.foldexpr='nvim_treesitter#foldexpr()'
