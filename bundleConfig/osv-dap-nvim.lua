042783f4e7671cb6bcc86d1d18278a263e509d2f
przepompownia <przepompownia@users.noreply.github.com>
2022-02-24 01:42:17 +0100

    Add OSVLaunch command

---
+++ bundleConfig/osv-dap-nvim.lua
@@ -0,0 +1,10 @@
+local osv = require('osv')
+local api = vim.api
+
+api.nvim_add_user_command('OSVLaunch', function()
+  osv.launch {
+    host = '127.0.0.1',
+    port = 9004,
+    log = '/tmp/osv.log',
+  }
+end, {nargs = 0})
