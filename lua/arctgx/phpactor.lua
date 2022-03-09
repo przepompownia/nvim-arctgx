2c634054383e7d48de2e43875e0a93b150fb63ac
przepompownia <przepompownia@users.noreply.github.com>
2022-03-09 02:25:24 +0100

    phpactor: class_new: try to wait on the server

--- lua/arctgx/phpactor.lua
+++ lua/arctgx/phpactor.lua
@@ -32,28 +32,43 @@ local function new_class_from_file(path)
 
   local buf = api.nvim_create_buf(true, false)
   api.nvim_buf_set_name(buf, path)
-  vim.api.nvim_buf_set_option(buf, 'filetype', 'php')
+  api.nvim_buf_set_option(buf, 'filetype', 'php')
+  vim.fn.bufload(buf)
 
-  vim.ui.select(
-    extension.get_variants(),
-    {
-      prompt = '\nSelect variant: ',
-    },
-    function (variant)
-      if nil == variant then
-        api.nvim_buf_delete(buf, {})
-        vim.notify('aborted')
+  local timer = vim.loop.new_timer()
+  local i = 1
+  timer:start(0, 1000, vim.schedule_wrap(function()
+    vim.lsp.for_each_buffer_client(buf, function(client, client_id, bufnr)
+      if 'phpactor' == client.name then
+        timer:close()
+
+        vim.ui.select(
+          extension.get_variants(),
+          {
+            prompt = '\nSelect variant: ',
+          },
+          function (variant)
+            if nil == variant then
+              api.nvim_buf_delete(buf, {})
+              vim.notify('aborted')
+              return
+            end
+
+            base.tab_drop_path(path)
+            vim.lsp.buf.execute_command({command = 'create_class', arguments = { vim.uri_from_fname(path), variant }})
+          end
+        )
         return
       end
-
-      vim.lsp.buf.execute_command({command = 'create_class', arguments = { vim.uri_from_fname(path), variant }})
-      base.tab_drop_path(path)
-    end
-  )
+    end)
+    vim.notify(('Server not ready, trying %s time'):format(tostring(i)))
+    i = i + 1
+  end))
 end
 
 function extension.class_new()
-  vim.ui.input({prompt = 'File: ', completion = 'file', default = api.nvim_buf_get_name(0)}, new_class_from_file)
+  local bufname = api.nvim_buf_get_name(0)
+  vim.ui.input({prompt = 'File: ', completion = 'file', default = bufname == '' and vim.loop.cwd() or bufname}, new_class_from_file)
 end
 
 return extension
