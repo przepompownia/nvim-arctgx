ebe40c643e3976137e08849967c74ed2ede50c9f
przepompownia <przepompownia@users.noreply.github.com>
2022-04-11 01:54:49 +0200

    Luasnip: add example snippets

--- bundleConfig/luasnip.lua
+++ bundleConfig/luasnip.lua
@@ -1,1 +1,24 @@
 require('luasnip.loaders.from_snipmate').load()
+
+local ls = require"luasnip"
+local s = ls.snippet
+-- local sn = ls.snippet_node
+-- local isn = ls.indent_snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+-- local f = ls.function_node
+-- local c = ls.choice_node
+-- local d = ls.dynamic_node
+-- local r = ls.restore_node
+-- local events = require("luasnip.util.events")
+
+ls.add_snippets('lua', {
+  s('lvd', {
+    t('print(vim.inspect('),
+    i(1),
+    t('))'),
+  }),
+  s('lvt', {
+    t('print(debug.traceback())'),
+  })
+})
