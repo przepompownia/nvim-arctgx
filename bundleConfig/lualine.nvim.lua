69a53350668446e37167b734ad973fff219e41ae
przepompownia <przepompownia@users.noreply.github.com>
2022-05-09 18:52:35 +0200

    lualine: show flie names in tabs

--- bundleConfig/lualine.nvim.lua
+++ bundleConfig/lualine.nvim.lua
@@ -1,1 +1,10 @@
-require('lualine').setup()
+require('lualine').setup({
+  tabline = {
+    lualine_a = {{'tabs', mode = 2}},
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {},
+  }
+})
