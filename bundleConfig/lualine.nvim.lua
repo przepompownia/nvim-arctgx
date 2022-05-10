7608f6c8688afe3a248639c6625bb8505cdf225a
przepompownia <przepompownia@users.noreply.github.com>
2022-05-10 14:37:52 +0200

    lualine: max_length

--- bundleConfig/lualine.nvim.lua
+++ bundleConfig/lualine.nvim.lua
@@ -1,10 +1,12 @@
 require('lualine').setup({
   tabline = {
-    lualine_a = {{'tabs', mode = 2}},
-    lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = {},
+    lualine_a = {{
+      'tabs',
+      mode = 2,
+      max_length = vim.o.columns,
+      fmt = function (name)
+        return name
+      end
+    }},
   }
 })
