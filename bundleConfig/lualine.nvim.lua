97e29e7ddf9c255ddc6ae50c4d5e5ac0c66e2ee7
przepompownia <przepompownia@users.noreply.github.com>
2022-05-10 19:16:48 +0200

    lualine: show no filename extensions

--- bundleConfig/lualine.nvim.lua
+++ bundleConfig/lualine.nvim.lua
@@ -1,10 +1,37 @@
+local function formatFilename(name)
+  return name:gsub('[.][^.]+$', '')
+end
+
 require('lualine').setup({
+  options = {
+    disabled_filetypes = {
+      'fern',
+      'dap-repl',
+    },
+  },
+  sections = {
+    lualine_c = {
+      {
+        'filename',
+        fmt = formatFilename,
+      }
+    },
+  },
+  -- inactive_sections = {
+  -- },
   tabline = {
     lualine_a = {{
       'tabs',
       mode = 2,
       max_length = vim.o.columns,
       tabs_color = {
+        -- inactive = function ()
+        --   return {
+        --     fg = '#37660C',
+        --     bg = '#989898',
+        --     -- link =
+        --   }
+        -- end,
         active = function ()
           return {
             fg = '#37660C',
@@ -13,9 +40,7 @@ require('lualine').setup({
           }
         end,
       },
-      fmt = function (name)
-        return name
-      end
+      fmt = formatFilename,
     }},
   }
 })
