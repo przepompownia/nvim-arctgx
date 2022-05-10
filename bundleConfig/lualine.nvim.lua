858af833cdb1bcb3ed30036cfcd639081b698e4b
przepompownia <przepompownia@users.noreply.github.com>
2022-05-10 16:07:44 +0200

    lualine: add example color setings

--- bundleConfig/lualine.nvim.lua
+++ bundleConfig/lualine.nvim.lua
@@ -4,6 +4,15 @@ require('lualine').setup({
       'tabs',
       mode = 2,
       max_length = vim.o.columns,
+      tabs_color = {
+        active = function ()
+          return {
+            fg = '#37660C',
+            bg = '#989898',
+            -- link =
+          }
+        end,
+      },
       fmt = function (name)
         return name
       end
