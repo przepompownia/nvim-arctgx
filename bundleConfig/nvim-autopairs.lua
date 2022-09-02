a1b53e551da80515b309d20f2ba79b13525a4ead
__ <__@__>
2022-09-02 23:03:02 +0200

    nvim-autopairs: disable backticks for SQL (sqls adds them)

--- bundleConfig/nvim-autopairs.lua
+++ bundleConfig/nvim-autopairs.lua
@@ -1,4 +1,26 @@
-require('nvim-autopairs').setup{}
+local npairs = require('nvim-autopairs')
+---@class Rule
+local Rule = require('nvim-autopairs.rule')
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 local cmp = require('cmp')
-cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
+local cond = require('nvim-autopairs.conds')
+
+npairs.setup {
+  -- disable_filetype = {
+  --   'sql',
+  --   'mysql',
+  -- }
+}
+
+---@type table<string, Rule>
+local existingRules = {
+   ['`'] = npairs.get_rule('`'),
+}
+
+for start, _ in pairs(existingRules) do
+  npairs.remove_rule(start)
+end
+
+npairs.add_rules({
+  existingRules['`']:with_pair(cond.not_filetypes({ 'sql', 'mysql' })),
+})
