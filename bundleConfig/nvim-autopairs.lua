82ac4edf94be4a6889deb7093a73cab90e706d49
__ <__@__>
2022-09-09 19:46:14 +0200

    nvim-autopairs: treesitter-php: precise what is type within a comment

--- bundleConfig/nvim-autopairs.lua
+++ bundleConfig/nvim-autopairs.lua
@@ -4,6 +4,7 @@ local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 local cmp = require('cmp')
 local cond = require('nvim-autopairs.conds')
 local ts_conds = require('nvim-autopairs.ts-conds')
+local treesitter = require('arctgx.treesitter')
 
 npairs.setup {
   check_ts = true,
@@ -21,10 +22,18 @@ for start, _ in pairs(existingRules) do
   npairs.remove_rule(start)
 end
 
+local function isAfterTypeInPhpDocblock()
+  local captures = treesitter.getCapturesBeforeCursor(0)
+  if not vim.tbl_contains(captures, 'comment') then
+    return false
+  end
+
+  return vim.tbl_contains(captures, 'type')
+end
 
 npairs.add_rules({
   existingRules['`']:with_pair(cond.not_filetypes({ 'sql', 'mysql' })),
-  Rule('<', '>', 'php'):with_pair(ts_conds.is_ts_node({ 'tag' })),
+  Rule('<', '>', 'php'):with_pair(isAfterTypeInPhpDocblock),
 })
 
 cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
---
+++ lua/arctgx/treesitter.lua
@@ -0,0 +1,20 @@
+local extension = {}
+
+--- from https://github.com/neovim/neovim/blob/95fd1ad83e24bbb14cc084fb001251939de6c0a9/runtime/lua/vim/treesitter.lua#L257
+function extension.getCapturesBeforeCursor(winnr)
+  winnr = winnr or 0
+  local bufnr = vim.api.nvim_win_get_buf(winnr)
+  local cursor = vim.api.nvim_win_get_cursor(winnr)
+
+  local data = vim.treesitter.get_captures_at_position(bufnr, cursor[1] - 1, math.max(0, cursor[2] - 1))
+
+  local captures = {}
+
+  for _, capture in ipairs(data) do
+    table.insert(captures, capture.capture)
+  end
+
+  return captures
+end
+
+return extension
