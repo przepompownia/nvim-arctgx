c9677b969b4db845a68109f8127d367bafadaa4b
__ <__@__>
2022-09-19 02:35:15 +0200

    Add LSPSignatureClearNamespace

--- bundleConfig/lsp_signature.lua
+++ bundleConfig/lsp_signature.lua
@@ -1,1 +1,12 @@
 require "lsp_signature".setup()
+local api = vim.api
+
+api.nvim_create_user_command(
+  'LSPSignatureClearNamespace',
+  function(opts)
+    api.nvim_buf_clear_namespace(0, api.nvim_get_namespaces()['lsp_signature_vt'], 0, -1)
+  end,
+  {
+    nargs = 0,
+  }
+)
