35a8afaa6e2f2dae6ea212fc9ee6982da996af25
__ <__@__>
2022-10-17 14:07:39 +0200

    lua: dump: wrap with `vim.schedule()`

--- plugin/dump.lua
+++ plugin/dump.lua
@@ -1,19 +1,21 @@
 function dump(value)
-  local buf = vim.api.nvim_create_buf(false, false)
+  vim.schedule(function ()
+    local buf = vim.api.nvim_create_buf(false, false)
 
-  local value_string = vim.inspect(value):gsub('<function (%d+)>', '"function %1"'):gsub('<%d+>', '')
+    local value_string = vim.inspect(value):gsub('<function (%d+)>', '"function %1"'):gsub('<%d+>', '')
 
-  local lines = vim.split('out = ' .. value_string, '\n')
-  vim.api.nvim_buf_set_lines(buf, 0, #lines, false, lines)
-  vim.api.nvim_buf_set_option(buf, 'filetype', 'lua')
-  vim.api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
-  vim.api.nvim_open_win(buf, true, {
-    relative = 'editor',
-    width = 120,
-    height = #lines,
-    row = 0.9,
-    col = 0.9,
-    border = 'rounded',
-    style = 'minimal',
-  })
+    local lines = vim.split('out = ' .. value_string, '\n')
+    vim.api.nvim_buf_set_lines(buf, 0, #lines, false, lines)
+    vim.api.nvim_buf_set_option(buf, 'filetype', 'lua')
+    vim.api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
+    vim.api.nvim_open_win(buf, true, {
+      relative = 'editor',
+      width = 120,
+      height = #lines,
+      row = 0.9,
+      col = 0.9,
+      border = 'rounded',
+      style = 'minimal',
+    })
+  end)
 end
