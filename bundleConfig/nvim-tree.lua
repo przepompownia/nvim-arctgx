d38bbd2cb4e559682ff123f86da89353ae370cd3
przepompownia <przepompownia@users.noreply.github.com>
2022-10-21 02:31:33 +0200

    nvim-tree: configure arrow keys

--- bundleConfig/nvim-tree.lua
+++ bundleConfig/nvim-tree.lua
@@ -1,7 +1,8 @@
 local keymap = require('vim.keymap')
 local treeapi = require('nvim-tree.api')
 local git = require('arctgx.git')
-local base= require('arctgx.base')
+local base = require('arctgx.base')
+local ntApi = require('nvim-tree.api')
 
 require('nvim-tree').setup({
   on_attach = function(bufnr)
@@ -9,53 +10,71 @@ require('nvim-tree').setup({
     -- vim.wo.cursorline = 1
     local inject_node = require('nvim-tree.utils').inject_node
 
-    vim.keymap.set("n", "<CR>", inject_node(function(node)
-      if node then
-        base.tab_drop_path(node.absolute_path)
-      end
-    end), { buffer = bufnr, noremap = true })
+    vim.keymap.set(
+      'n',
+      '<CR>',
+      inject_node(function(node)
+        if 1 == vim.fn.isdirectory(node.absolute_path) then
+          base.tab_drop_path(node.absolute_path)
+        end
+      end),
+      {buffer = bufnr, noremap = true}
+    )
+    vim.keymap.set(
+      'n',
+      '<Right>',
+      inject_node(function(node)
+        if node.nodes then
+          require('nvim-tree.lib').expand_or_collapse(node)
+        end
+      end),
+      {buffer = bufnr, noremap = true}
+    )
+    vim.keymap.set(
+      'n',
+      '<Left>',
+      ntApi.node.navigate.parent_close,
+      {buffer = bufnr, noremap = true}
+    )
   end,
   sort_by = 'case_insensitive',
   view = {
-    adaptive_size = true,
+    mappings = {
+      list = {
+        {
+          key = '<Tab>',
+          action = '',
+        },
+      },
+    },
   },
   renderer = {
     group_empty = true,
     icons = {
       glyphs = {
-        default = "●",
-        symlink = "",
-        bookmark = "",
+        default = '●',
+        symlink = '',
+        bookmark = '',
         folder = {
-          arrow_closed = "▶",
-          arrow_open = "▼",
-          default = "",
-          open = " ",
-          empty = "",
-          empty_open = "",
-          symlink = "",
-          symlink_open = "",
+          arrow_closed = '▶',
+          arrow_open = '▼',
+          default = '',
+          open = ' ',
+          empty = '',
+          empty_open = '',
+          symlink = '',
+          symlink_open = '',
         },
       },
     },
   },
-  filters = {
-    dotfiles = true,
-  },
+  filters = {dotfiles = true},
 })
 
 local function focusOnFile()
   treeapi.tree.toggle(true, false, git.top(vim.fs.dirname(vim.api.nvim_buf_get_name(0))))
 end
--- vim.api.nvim_create_augroup('NvimTreeSettings', {clear = true})
--- vim.api.nvim_create_autocmd('FileType', {
---   pattern = 'NvimTree',
---   group = 'NvimTreeSettings',
---   callback = function ()
---     vim.wo.cursorline = 1
---     print(vim.wo.cursorline)
---   end,
--- })
+
 -- @todo
 -- mappings
 -- icons
