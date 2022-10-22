db7b1d7149a7e7204ed8831753c14a8d58e262ab
przepompownia <przepompownia@users.noreply.github.com>
2022-10-22 16:50:00 +0200

    loadKeyToPlugMappings: use `mode` from loop

--- lua/arctgx/vim/keymap.lua
+++ lua/arctgx/vim/keymap.lua
@@ -21,7 +21,7 @@ function extension.loadKeyToPlugMappings(mappings, bufnr)
     for _, mode in ipairs(modes) do
       keymap.set({ mode }, lhs, function()
         local internalSeq = vim.api.nvim_replace_termcodes(mapping.rhs, true, false, true)
-        vim.api.nvim_feedkeys(internalSeq, 'n', false)
+        vim.api.nvim_feedkeys(internalSeq, mode, false)
         if true == mapping.repeatable then
           repeatSeq(internalSeq)
         end
