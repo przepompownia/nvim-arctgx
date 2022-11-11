6560369e95d7199f3a88a5e3afd3146a9a65c03f
__ <__@__>
2022-11-11 18:51:30 +0100

    Add basic noice.nvim setup

---
+++ bundleConfig/noice.nvim.lua
@@ -0,0 +1,15 @@
+require('noice').setup({
+  lsp = {
+    override = {
+      -- ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      -- ["vim.lsp.util.stylize_markdown"] = true,
+      -- ["cmp.entry.get_documentation"] = true,
+    },
+    signature = {
+      enabled = false,
+    },
+    hover = {
+      enabled = false,
+    }
+  },
+})
