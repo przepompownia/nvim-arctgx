4c3686767e772ac16ebd1f127b075f818e38653c
przepompownia <przepompownia@users.noreply.github.com>
2022-12-02 17:33:35 +0100

    Move switching bg autocmd to `plugin`
    - use vim.g.colorscheme{Dark,Light}

--- bundleConfig/colorscheme-modus.lua
+++ bundleConfig/colorscheme-modus.lua
@@ -1,18 +1,2 @@
-local api = vim.api
 vim.g.modus_dim_inactive_window = 0
-api.nvim_create_augroup('ColorschemeLoading', {clear = true})
-api.nvim_create_autocmd('OptionSet', {
-  group = 'ColorschemeLoading',
-  pattern = 'background',
-  nested = true,
-  callback = function ()
-    local function theme()
-      if vim.v.option_new == 'light' then
-        return 'modus-operandi'
-      end
-
-      return 'modus-vivendi'
-    end
-    api.nvim_cmd({cmd = 'colorscheme', args = {theme()}}, {})
-  end
-})
+vim.g.colorschemeLight = 'modus-operandi'
---
+++ plugin/colorscheme.lua
@@ -0,0 +1,17 @@
+local api = vim.api
+api.nvim_create_augroup('ColorschemeLoading', {clear = true})
+api.nvim_create_autocmd('OptionSet', {
+  group = 'ColorschemeLoading',
+  pattern = 'background',
+  nested = true,
+  callback = function ()
+    local function theme()
+      if vim.v.option_new == 'light' then
+        return vim.g.colorschemeLight or 'delek'
+      end
+
+      return vim.g.colorschemeDark or 'darkblue'
+    end
+    api.nvim_cmd({cmd = 'colorscheme', args = {theme()}}, {})
+  end
+})
