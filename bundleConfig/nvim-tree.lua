42cea8c2f10a0ccf19e6f47c46cdc69afd7d2b31
przepompownia <przepompownia@users.noreply.github.com>
2022-12-11 18:12:16 +0100

    nvim-tree: remove withdrawn option

--- bundleConfig/nvim-tree.lua
+++ bundleConfig/nvim-tree.lua
@@ -10,7 +10,6 @@ vim.g.loaded_netrwPlugin = 1
 require('nvim-tree').setup({
   hijack_cursor = true,
   remove_keymaps = {'<Tab>'},
-  create_in_closed_folder = true,
   actions = {
     change_dir = {
       enable = false,
