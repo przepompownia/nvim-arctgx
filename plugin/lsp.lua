9efa80a3c34d3c1f13ca6cda6896a887480c2e15
przepompownia <przepompownia@users.noreply.github.com>
2023-01-06 14:36:07 +0100

    Set rounded border around LSP floating windows

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -1,1 +1,9 @@
 vim.lsp.set_log_level(vim.log.levels.WARN)
+
+local border = 'rounded'
+local origUtilOpenFloatingPreview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+  opts = opts or {}
+  opts.border = opts.border or border
+  return origUtilOpenFloatingPreview(contents, syntax, opts, ...)
+end
