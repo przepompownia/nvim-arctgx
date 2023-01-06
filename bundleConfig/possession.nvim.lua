4a2b30441407c9570bfff22e44aecec7b6b5fff6
przepompownia <przepompownia@users.noreply.github.com>
2023-01-07 00:34:55 +0100

    Configure possession.nvim

---
+++ bundleConfig/possession.nvim.lua
@@ -0,0 +1,13 @@
+local session = require('arctgx.session')
+require('possession').setup {
+  hooks = {
+    before_save = function ()
+      session.runBeforeSaveHooks()
+      return {}
+    end,
+  },
+  plugins = {
+    nvim_tree = false,
+    delete_hidden_buffers = false,
+  },
+}
