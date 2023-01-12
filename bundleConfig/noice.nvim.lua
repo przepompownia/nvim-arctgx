de7c4613eed47d6ab84b99fff6d47bf72e1f0891
przepompownia <przepompownia@users.noreply.github.com>
2023-01-12 02:24:26 +0100

    noice: enable signature

--- bundleConfig/noice.nvim.lua
+++ bundleConfig/noice.nvim.lua
@@ -16,7 +16,7 @@ require('noice').setup({
       -- ["cmp.entry.get_documentation"] = true,
     },
     signature = {
-      enabled = false,
+      enabled = true,
       auto_open = {
         enabled = true,
         trigger = true,
