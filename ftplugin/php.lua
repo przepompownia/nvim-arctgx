f9713965d1086201bdc4f2933615136ed822a60c
przepompownia <przepompownia@users.noreply.github.com>
2023-01-22 02:08:00 +0100

    filetype/php.{vim -> lua}

---
+++ ftplugin/php.lua
@@ -0,0 +1,8 @@
+vim.opt_local.iskeyword:remove('$')
+vim.opt_local.spell = true
+vim.opt_local.wrap = false
+vim.bo.softtabstop = 4
+vim.bo.shiftwidth = 4
+vim.bo.expandtab = true
+vim.keymap.set('i', '<A-.>', '->', { buffer = true })
+vim.keymap.set('i', '<A-char-62>', '=>', { buffer = true })
--- ftplugin/php.vim
+++
@@ -1,6 +0,0 @@
-setlocal iskeyword-=$
-setlocal nowrap
-setlocal softtabstop=4 shiftwidth=4 expandtab
-inoremap <buffer> <A-.> ->
-inoremap <buffer> <A-char-62> =>
-setlocal spell
