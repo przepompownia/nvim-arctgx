aab8832d3908a2ffcd05bc87b6d994d7595a9506
przepompownia <przepompownia@users.noreply.github.com>
2023-02-03 01:07:55 +0100

    viml -> lua: fugitive config

---
+++ bundleConfig/fugitive.lua
@@ -0,0 +1,45 @@
+local api = vim.api
+
+local augroup = api.nvim_create_augroup('FugitiveConfig', {clear = true})
+api.nvim_create_autocmd('BufReadPost', {
+  pattern = 'fugitive://*',
+  callback = function (params)
+    api.nvim_buf_set_option(params.buf, 'bufhidden', 'delete')
+  end
+})
+api.nvim_create_autocmd('User', {
+  pattern = 'FugitiveChanged',
+  callback = function (params)
+    api.nvim_exec_autocmds('User', {pattern = 'ChangeIdeStatus', modeline = false})
+  end
+})
+api.nvim_create_autocmd('FileType', {
+  pattern = 'gitcommit',
+  callback = function (params)
+    vim.cmd([[
+      inoremap <buffer> <F3> <C-\><C-n>:q<CR>
+    ]])
+  end
+})
+api.nvim_create_autocmd('FileType', {
+  pattern = {'git', 'fugitiveblame', 'fugitive'},
+  callback = function (params)
+   vim.keymap.set('n', 'q', vim.cmd.quit)
+  end
+})
+vim.keymap.set('n', '<Plug>(ide-git-status)', function ()
+  vim.cmd.Git()
+  vim.cmd.resize(33)
+end)
+vim.keymap.set('n', '<Plug>(ide-git-log)', function ()
+  vim.cmd.Gllog()
+end)
+vim.keymap.set('n', '<Plug>(ide-git-push-all)', function ()
+  vim.cmd.Git {'pushall'}
+end)
+vim.keymap.set('n', '<Plug>(ide-git-commit)', function ()
+  vim.cmd.Git {'commit'}
+end)
+vim.keymap.set('n', '<Plug>(ide-git-blame)', function ()
+  vim.cmd.Git {'blame'}
+end)
