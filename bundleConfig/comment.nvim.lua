034601f1636dfa07e6400fa4f2f53bcd194e4e6c
przepompownia <przepompownia@users.noreply.github.com>
2023-02-11 03:11:03 +0100

    comment.nvim: mappings: use examples from api doc

--- bundleConfig/comment.nvim.lua
+++ bundleConfig/comment.nvim.lua
@@ -1,3 +1,5 @@
+local cmapi = require('Comment.api')
+
 local function createPrehook()
   local res, comment = pcall(require, 'ts_context_commentstring.integrations.comment_nvim')
 
@@ -12,11 +14,16 @@ require('Comment').setup {
   pre_hook = createPrehook(),
 }
 
-vim.cmd([[
-  nmap <C-_> gccj
-  imap <C-_> <C-o>gcc<C-o>j
-  vmap <C-_> gc
-  nmap <C-/> gccj
-  imap <C-/> <C-o>gcc<C-o>j
-  vmap <C-/> gc
-]])
+vim.keymap.set({'n', 'i'}, '<C-/>', function ()
+  cmapi.toggle.linewise.current()
+  vim.api.nvim_feedkeys('j', 'n', false)
+end)
+
+local esc = vim.api.nvim_replace_termcodes(
+    '<ESC>', true, false, true
+)
+
+vim.keymap.set('x', '<C-/>', function()
+    vim.api.nvim_feedkeys(esc, 'nx', false)
+    cmapi.toggle.linewise(vim.fn.visualmode())
+end)
