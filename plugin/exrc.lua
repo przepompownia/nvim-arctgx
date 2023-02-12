60cc4b94f195a624d66b02fc1f408f2c5b4a56e4
przepompownia <przepompownia@users.noreply.github.com>
2023-02-12 03:11:54 +0100

    exrc: skip before v0.9

--- plugin/exrc.lua
+++ plugin/exrc.lua
@@ -2,6 +2,9 @@ vim.api.nvim_create_augroup('ArctgxExrc', {clear = true})
 vim.api.nvim_create_autocmd('VimEnter', {
   group = 'ArctgxExrc',
   callback = function ()
+    if not vim.fn.has('nvim-0.9') then
+      return
+    end
     local file = vim.loop.cwd() .. '/.exrc.lua'
     if nil == vim.secure.read(file) then
       return
