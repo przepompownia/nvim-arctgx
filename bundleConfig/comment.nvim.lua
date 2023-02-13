18ff77f8c036e06f13fc52a5e23eecb9686e1de9
przepompownia <przepompownia@users.noreply.github.com>
2023-02-14 00:08:36 +0100

    comment: add working shortcut for terminal

--- bundleConfig/comment.nvim.lua
+++ bundleConfig/comment.nvim.lua
@@ -14,16 +14,18 @@ require('Comment').setup {
   pre_hook = createPrehook(),
 }
 
-vim.keymap.set({'n', 'i'}, '<C-/>', function ()
-  cmapi.toggle.linewise.current()
-  vim.api.nvim_feedkeys('j', 'n', false)
-end)
-
 local esc = vim.api.nvim_replace_termcodes(
-    '<ESC>', true, false, true
+  '<ESC>', true, false, true
 )
 
-vim.keymap.set('x', '<C-/>', function()
+for _, lhs in ipairs({'<C-/>', '<C-_>'}) do
+  vim.keymap.set({'n', 'i'}, lhs, function ()
+    cmapi.toggle.linewise.current()
+    vim.api.nvim_feedkeys('j', 'n', false)
+  end)
+
+  vim.keymap.set('x', lhs, function()
     vim.api.nvim_feedkeys(esc, 'nx', false)
     cmapi.toggle.linewise(vim.fn.visualmode())
-end)
+  end)
+end
