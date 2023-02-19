e1bededf2c23aa3d1a4cfd825ae8b4f0924adf4d
przepompownia <przepompownia@users.noreply.github.com>
2023-02-19 02:25:40 +0100

    viml -> lua: shell

---
+++ plugin/shell.lua
@@ -0,0 +1,20 @@
+local augroup = vim.api.nvim_create_augroup('terminalMode', {clear = true})
+vim.api.nvim_create_autocmd('TermOpen', {
+  group = augroup,
+  pattern = 'term://*',
+  callback = function ()
+    vim.opt_local.number = false
+    vim.cmd.startinsert()
+  end
+})
+vim.api.nvim_create_autocmd('TermClose', {
+  group = augroup,
+  pattern = 'term://*',
+  callback = function ()
+    vim.cmd.quit()
+  end
+})
+
+vim.api.nvim_create_user_command('Shell', function ()
+  vim.fn['arctgx#arctgx#openShell'](vim.fn.fnamemodify(vim.fn.bufname(''), ':h'))
+end, {})
--- plugin/shell.vim
+++
@@ -1,7 +0,0 @@
-command Shell call arctgx#arctgx#openShell(fnamemodify(bufname(''), ':h'))
-
-augroup terminalMode
-  autocmd!
-  autocmd TermOpen ++nested term://* setlocal nonumber | startinsert
-  autocmd TermClose ++nested term://* quit
-augroup END
