cc4dc1ab53f08cbde682bd52615c411474ce44b2
przepompownia <przepompownia@users.noreply.github.com>
2023-02-22 01:45:15 +0100

    Drop bundle.vim

--- lua/arctgx/plugin.lua
+++ lua/arctgx/plugin.lua
@@ -1,8 +1,18 @@
 local plugin = {}
 
+local function listAllPlugins(pluginDir)
+  local plugins = {}
+  local pluginPaths = vim.fn.globpath(pluginDir, '*', 1, 1)
+  for _, path in ipairs(pluginPaths) do
+    table.insert(plugins, vim.fn.fnamemodify(path, ':t'))
+  end
+  
+  return plugins
+end
+
 function plugin.loadCustomConfiguration(pluginDirs, pluginConfigDir)
   for _, pluginDir in ipairs(pluginDirs) do
-    for _, pluginName in ipairs(vim.fn['arctgx#bundle#listAllBundles'](pluginDir)) do
+    for _, pluginName in ipairs(listAllPlugins(pluginDir)) do
       if vim.tbl_contains(vim.opt.runtimepath:get(), vim.loop.fs_realpath(pluginDir .. '/' .. pluginName)) then
         plugin.loadSingleConfiguration(pluginName, pluginConfigDir)
       end
