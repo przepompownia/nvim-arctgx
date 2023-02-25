ebed07ecaa621899444c7e09d49884de736064a2
przepompownia <przepompownia@users.noreply.github.com>
2023-02-25 17:28:05 +0100

    fixup! nvim-lspconfig: use `exepath` for phpactor

--- pluginConfigs/nvim-lspconfig.lua
+++ pluginConfigs/nvim-lspconfig.lua
@@ -37,7 +37,7 @@ configs.phpactor = {
     },
     cmd = {
       -- 'phpxx',
-      vim.fn.exepath('phpactor')
+      vim.fn.exepath('phpactor'),
       'language-server',
       -- '-vvv',
     },
