4922f64f8c316e83665181eda12c41450ea8de6c
przepompownia <przepompownia@users.noreply.github.com>
2023-02-27 15:26:04 +0100

    neogit commands: change cwd to git topdir

--- pluginConfigs/neogit.lua
+++ pluginConfigs/neogit.lua
@@ -3,12 +3,16 @@ local neogit = require('neogit')
 
 neogit.setup {}
 
+local function getTopDir()
+  return require('arctgx.git').top(base.getBufferCwd())
+end
+
 vim.keymap.set('n', '<Plug>(ide-git-status)', function ()
-  neogit.open({ cwd = base.getBufferCwd() })
+  neogit.open({ cwd = getTopDir() })
 end)
 vim.keymap.set('n', '<Plug>(ide-git-commit)', function ()
-  neogit.open({ 'commit', cwd = base.getBufferCwd()})
+  neogit.open({ 'commit', cwd = getTopDir()})
 end)
 vim.keymap.set('n', '<Plug>(ide-git-push-all)', function ()
-  neogit.open({ 'push', cwd = base.getBufferCwd()})
+  neogit.open({ 'push', cwd = getTopDir()})
 end)
