f1910e8f6eb99eb23037ad866a482d2259d6fc5a
przepompownia <przepompownia@users.noreply.github.com>
2023-02-28 21:37:05 +0100

    help: workaround not triggered WinClosed for `<C-w>T` on help file

--- ftplugin/help.lua
+++ ftplugin/help.lua
@@ -6,4 +6,6 @@ vim.keymap.set({'n'}, 'q', function ()
   vim.api.nvim_buf_delete(0, {})
 end, {buffer = 0})
 
+-- WinClosed not triggered
+require('arctgx.windowhistory').getInstance():remove(vim.api.nvim_get_current_win())
 vim.cmd([[silent wincmd T]])
