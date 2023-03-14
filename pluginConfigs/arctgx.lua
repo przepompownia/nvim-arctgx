ca87db65fb5c4006f13349ff8dc52aa4d1e12757
przepompownia <przepompownia@users.noreply.github.com>
2023-03-14 04:03:29 +0100

    Map to git.pushToAllRemoteRepos

--- pluginConfigs/arctgx.lua
+++ pluginConfigs/arctgx.lua
@@ -1,4 +1,5 @@
-local writebackup = require 'arctgx.writebackup'
-
 vim.g.arctgxBackupDir = vim.env['XDG_CONFIG_HOME'] .. '/.config/backups'
-vim.keymap.set('n', '<Plug>(ide-write-backup)', writebackup.save)
+vim.keymap.set('n', '<Plug>(ide-write-backup)', require('arctgx.writebackup').save)
+vim.keymap.set('n', '<Plug>(ide-git-push-all)', function ()
+  require'arctgx.git'.pushToAllRemoteRepos(require('arctgx.base').getBufferCwd())
+end)
