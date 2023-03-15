bda2d9efc371f2e9c8b07cc75d61c87a6e429643
przepompownia <przepompownia@users.noreply.github.com>
2023-03-15 01:35:34 +0100

    lazy

--- pluginConfigs/arctgx.lua
+++ pluginConfigs/arctgx.lua
@@ -1,5 +1,7 @@
 vim.g.arctgxBackupDir = vim.env['XDG_CONFIG_HOME'] .. '/.config/backups'
-vim.keymap.set('n', '<Plug>(ide-write-backup)', require('arctgx.writebackup').save)
+vim.keymap.set('n', '<Plug>(ide-write-backup)', function ()
+  require('arctgx.writebackup').save()
+end)
 vim.keymap.set('n', '<Plug>(ide-git-push-all)', function ()
   require'arctgx.git'.pushToAllRemoteRepos(require('arctgx.base').getBufferCwd())
 end)
