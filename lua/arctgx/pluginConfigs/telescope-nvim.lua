f59e114eed99981992adc5250141e4a8f62687be
przepompownia <przepompownia@users.noreply.github.com>
2023-03-28 00:45:10 +0200

    Lazy

--- lua/arctgx/pluginConfigs/telescope-nvim.lua
+++ lua/arctgx/pluginConfigs/telescope-nvim.lua
@@ -1,6 +1,5 @@
 local base = require 'arctgx.base'
 local api = vim.api
-local arctgx = require('arctgx.telescope')
 local keymap = require('vim.keymap')
 
 require('telescope').setup {
@@ -22,8 +21,8 @@ require('telescope').setup {
   },
 }
 
-api.nvim_create_user_command('GGrep', function(opts) arctgx.gitGrep(opts.args, false, false) end, {nargs = '*'})
-api.nvim_create_user_command('RGrep', function(opts) arctgx.rgGrep(opts.args, false, false) end, {nargs = '*'})
+api.nvim_create_user_command('GGrep', function(opts) require('arctgx.telescope').gitGrep(opts.args, false, false) end, {nargs = '*'})
+api.nvim_create_user_command('RGrep', function(opts) require('arctgx.telescope').rgGrep(opts.args, false, false) end, {nargs = '*'})
 api.nvim_create_user_command(
   'GTDiff',
   function(opts)
@@ -40,27 +39,27 @@ api.nvim_create_user_command(
     end,
   }
 )
-keymap.set('n', '<Plug>(ide-grep-git)', function() arctgx.gitGrep('', false, false) end)
-keymap.set('n', '<Plug>(ide-grep-files)', function() arctgx.rgGrep('', false, false) end)
-keymap.set('n', '<Plug>(ide-browse-files)', arctgx.filesAll)
-keymap.set('n', '<Plug>(ide-browse-gfiles)', arctgx.filesGit)
+keymap.set('n', '<Plug>(ide-grep-git)', function() require('arctgx.telescope').gitGrep('', false, false) end)
+keymap.set('n', '<Plug>(ide-grep-files)', function() require('arctgx.telescope').rgGrep('', false, false) end)
+keymap.set('n', '<Plug>(ide-browse-files)', function() require('arctgx.telescope').filesAll() end)
+keymap.set('n', '<Plug>(ide-browse-gfiles)', function() require('arctgx.telescope').filesGit() end)
 keymap.set('n', '<Plug>(ide-browse-cmd-history)', function() require('telescope.builtin').command_history() end)
-keymap.set('n', '<Plug>(ide-browse-history)', function() arctgx.oldfiles(false) end)
-keymap.set('n', '<Plug>(ide-browse-history-in-cwd)', function() arctgx.oldfiles(true) end)
-keymap.set('n', '<Plug>(ide-browse-buffers)', arctgx.buffers)
+keymap.set('n', '<Plug>(ide-browse-history)', function() require('arctgx.telescope').oldfiles(false) end)
+keymap.set('n', '<Plug>(ide-browse-history-in-cwd)', function() require('arctgx.telescope').oldfiles(true) end)
+keymap.set('n', '<Plug>(ide-browse-buffers)', function() require('arctgx.telescope').buffers() end)
 keymap.set('n', '<Plug>(ide-browse-windows)', function() require('arctgx.telescope.windows').list() end)
-keymap.set('n', '<Plug>(ide-git-show-branches)', arctgx.branches)
+keymap.set('n', '<Plug>(ide-git-show-branches)', function() require('arctgx.telescope').branches() end)
 keymap.set('v', '<Plug>(ide-git-string-search-operator)', function()
-  arctgx.gitGrep(base.getVisualSelection(), true)
+  require('arctgx.telescope').gitGrep(base.getVisualSelection(), true)
 end)
 keymap.set('v', '<Plug>(ide-grep-string-search-operator)', function()
-  arctgx.rgGrep(base.getVisualSelection(), true)
+  require('arctgx.telescope').rgGrep(base.getVisualSelection(), true)
 end)
 keymap.set('v', '<Plug>(ide-git-files-search-operator)', function()
-  arctgx.filesGit(base.getVisualSelection(), true)
+  require('arctgx.telescope').filesGit(base.getVisualSelection(), true)
 end)
 keymap.set('v', '<Plug>(ide-files-search-operator)', function()
-  arctgx.filesAll(base.getVisualSelection(), true)
+  require('arctgx.telescope').filesAll(base.getVisualSelection(), true)
 end)
 
 keymap.set('n', '<Plug>(ide-git-string-search-operator)', function ()
