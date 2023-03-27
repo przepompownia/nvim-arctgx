1406c1292e902348a6ae421a67e50f46d51e9f20
przepompownia <przepompownia@users.noreply.github.com>
2023-03-27 23:20:42 +0200

    nvim-tree: focus: do not use `dirname` on directory

--- lua/arctgx/pluginConfigs/nvim-tree.lua
+++ lua/arctgx/pluginConfigs/nvim-tree.lua
@@ -91,8 +91,8 @@ require('nvim-tree').setup({
 
 local function focusOnFile()
   local treeapi = require('nvim-tree.api')
-  local bufName = vim.loop.fs_realpath(vim.api.nvim_buf_get_name(0)) or vim.loop.cwd()
-  treeapi.tree.open(require('arctgx.git').top(vim.fs.dirname(bufName)))
+  local bufName = vim.loop.fs_realpath(vim.api.nvim_buf_get_name(0))
+  treeapi.tree.open(require('arctgx.git').top(bufName and vim.fs.dirname(bufName) or vim.loop.cwd()))
   -- treeapi.tree.toggle_hidden_filter()
   treeapi.live_filter.clear()
   treeapi.tree.find_file(bufName)
