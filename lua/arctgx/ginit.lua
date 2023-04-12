0fc495a9b372f75d902f36c25d5b960b1768ea4b
przepompownia <przepompownia@users.noreply.github.com>
2023-04-13 01:30:09 +0200

    nvim-qt: use GuiFont!

--- lua/arctgx/ginit.lua
+++ lua/arctgx/ginit.lua
@@ -1,8 +1,9 @@
-vim.opt_global.guifont = 'SauceCodePro Nerd Font Mono:style=Regular:h14'
+vim.opt_global.guifont = 'SauceCodePro Nerd Font Mono:h14'
 if vim.g.GuiLoaded then
   vim.fn.GuiWindowFullScreen(1)
   vim.cmd.GuiPopupmenu(false)
   vim.cmd.GuiTabline(false)
+  vim.cmd.GuiFont({args = {vim.opt_global.guifont:get()[1]}, bang = true})
   vim.cmd.packadd 'vim-tmux-focus-events'
   vim.env.TMUX = nil
 end
