adb1dc03883c786d163d4e7bd08ccc95b7ffaddd
przepompownia <przepompownia@users.noreply.github.com>
2023-10-17 14:31:26 +0200

    nord-nvim: add workaround for highlighting `@error`

--- lua/arctgx/pluginConfigs/nord-nvim.lua
+++ lua/arctgx/pluginConfigs/nord-nvim.lua
@@ -1,4 +1,7 @@
 vim.g.colorschemeDark = 'nord'
 require('nord').setup {
   diff = {mode = 'fg'},
+  on_highlights = function (highlights, _colors)
+    highlights['@error'] = {fg = '#880000'}
+  end,
 }
