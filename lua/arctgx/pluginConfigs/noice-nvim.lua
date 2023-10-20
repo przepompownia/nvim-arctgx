9365ab289d0acb15c40befdfaf60a7edf86ae0f4
przepompownia <przepompownia@users.noreply.github.com>
2023-10-20 15:24:17 +0200

    Defer loading Noice
    https://github.com/folke/noice.nvim/issues/628

--- lua/arctgx/pluginConfigs/noice-nvim.lua
+++ lua/arctgx/pluginConfigs/noice-nvim.lua
@@ -1,52 +1,59 @@
-require('noice').setup({
-  messages = {
-    view = 'mini',
-    view_search = false,
-    view_error = 'mini',
-    view_warn = 'mini',
-  },
-  notify = {
-    enabled = true,
-    view = 'mini',
-  },
-  lsp = {
-    override = {
-      -- ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-      -- ["vim.lsp.util.stylize_markdown"] = true,
-      -- ["cmp.entry.get_documentation"] = true,
+local function setupNoice()
+  require('noice').setup({
+    messages = {
+      view = 'mini',
+      view_search = false,
+      view_error = 'mini',
+      view_warn = 'mini',
     },
-    signature = {
+    notify = {
       enabled = true,
-      auto_open = {
-        enabled = true,
-        trigger = true,
-        luasnip = true,
-        throttle = 50,
+      view = 'mini',
+    },
+    lsp = {
+      override = {
+        -- ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        -- ["vim.lsp.util.stylize_markdown"] = true,
+        -- ["cmp.entry.get_documentation"] = true,
       },
-      view = nil, -- {
-      ---@type NoiceViewOptions
-      -- opts = {
-      --   win_options = { border = 'rounded' },
-      -- },
+      signature = {
+        enabled = false,
+        auto_open = {
+          enabled = true,
+          trigger = true,
+          luasnip = true,
+          throttle = 50,
+        },
+        view = nil, -- {
+        ---@type NoiceViewOptions
+        -- opts = {
+        --   win_options = { border = 'rounded' },
+        -- },
+      },
+      hover = {
+        enabled = false,
+        view = nil,
+        ---@type NoiceViewOptions
+        opts = {},
+      },
     },
-    hover = {
-      enabled = false,
-      view = nil,
-      ---@type NoiceViewOptions
-      opts = {},
+    views = {
+      mini = {
+        timeout = 5000,
+      },
     },
-  },
-  views = {
-    mini = {
-      timeout = 5000,
+    presets = {
+      lsp_doc_border = true,
     },
-  },
-  presets = {
-    lsp_doc_border = true,
-  },
+  })
+end
+
+vim.api.nvim_create_autocmd('UIEnter', {
+  once = true,
+  callback = setupNoice,
 })
 
-vim.keymap.set('c', '<A-CR>', function()
+vim.keymap.set('c', '<A-CR>', function ()
   require('noice').redirect(vim.fn.getcmdline())
   vim.api.nvim_input('<esc>')
-end, { desc = 'Redirect Cmdline' })
+end, {desc = 'Redirect Cmdline'})
