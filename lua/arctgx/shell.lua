eafa254d299e78518bd6d72d8b3f0456bc00ec4c
przepompownia <przepompownia@users.noreply.github.com>
2023-10-23 01:50:21 +0200

    shell.open: fix empty table as cmd

--- lua/arctgx/shell.lua
+++ lua/arctgx/shell.lua
@@ -3,10 +3,14 @@ local Shell = {}
 ---@param opts {cmd: string[], cwd: string}
 function Shell.open(opts)
   opts = opts or {}
+  vim.validate({cmd = {opts.cmd, {'table', 'string'}}})
+  if #opts.cmd == 0 then
+    opts.cmd = vim.opt.shell:get()
+  end
   local cwd = opts.cwd or vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ':h')
   vim.cmd.new({mods = {split = 'botright'}})
   vim.fn.termopen(
-    opts.cmd or vim.opt.shell:get(),
+    opts.cmd,
     {
       cwd = cwd,
     }
