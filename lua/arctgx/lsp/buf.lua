b3d3cbf87fb249fe400b0e5192aa90ec185ea419
przepompownia <przepompownia@users.noreply.github.com>
2023-10-31 23:52:48 +0100

    Use vim.lsp.buf_request_all for peek definition

--- lua/arctgx/lsp/buf.lua
+++ lua/arctgx/lsp/buf.lua
@@ -1,7 +1,7 @@
 local Buf = {}
 
-local function request(method, params, handler)
-  return vim.lsp.buf_request(0, method, params, handler)
+local function request(method, params, handler, bufnr)
+  return vim.lsp.buf_request_all(bufnr, method, params, handler)(0, method, params, handler)
 end
 
 local function previewLocation(_, result, ctx, _)
@@ -20,9 +20,9 @@ function Buf.workspaceFolders()
   dump(vim.lsp.buf.list_workspace_folders())
 end
 
-function Buf.peekDefinition()
+function Buf.peekDefinition(bufnr)
   local params = vim.lsp.util.make_position_params()
-  request('textDocument/definition', params, previewLocation)
+  request('textDocument/definition', params, previewLocation, bufnr)
 end
 
 return Buf
