19040c53be32f374068c428bb742ed5cbe3f598f
przepompownia <przepompownia@users.noreply.github.com>
2023-11-04 00:24:23 +0100

    nvim-treesitter-context: clean outdated settings

--- lua/arctgx/pluginConfigs/nvim-treesitter-context.lua
+++ lua/arctgx/pluginConfigs/nvim-treesitter-context.lua
@@ -2,20 +2,4 @@ require 'treesitter-context'.setup {
   enable = true,
   throttle = true,
   max_lines = 0,
-  patterns = {
-    default = {
-      'function',
-      'method',
-      -- 'for', -- These won't appear in the context
-      -- 'while',
-      -- 'if',
-      -- 'switch',
-      -- 'case',
-    },
-    -- Example for a specific filetype.
-    -- If a pattern is missing, *open a PR* so everyone can benefit.
-    --   rust = {
-    --       'impl_item',
-    --   },
-  },
 }
