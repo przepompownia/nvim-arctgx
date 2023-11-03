cc45864d72d2e854d0ec4302276d1e7fa8f1d02b
przepompownia <przepompownia@users.noreply.github.com>
2023-11-04 00:21:19 +0100

    Format

--- lua/arctgx/pluginConfigs/nvim-treesitter-context.lua
+++ lua/arctgx/pluginConfigs/nvim-treesitter-context.lua
@@ -1,21 +1,21 @@
-require'treesitter-context'.setup{
-    enable = true,
-    throttle = true,
-    max_lines = 0,
-    patterns = {
-        default = {
-            'function',
-            'method',
-            -- 'for', -- These won't appear in the context
-            -- 'while',
-            -- 'if',
-            -- 'switch',
-            -- 'case',
-        },
-        -- Example for a specific filetype.
-        -- If a pattern is missing, *open a PR* so everyone can benefit.
-        --   rust = {
-        --       'impl_item',
-        --   },
+require 'treesitter-context'.setup {
+  enable = true,
+  throttle = true,
+  max_lines = 0,
+  patterns = {
+    default = {
+      'function',
+      'method',
+      -- 'for', -- These won't appear in the context
+      -- 'while',
+      -- 'if',
+      -- 'switch',
+      -- 'case',
     },
+    -- Example for a specific filetype.
+    -- If a pattern is missing, *open a PR* so everyone can benefit.
+    --   rust = {
+    --       'impl_item',
+    --   },
+  },
 }
