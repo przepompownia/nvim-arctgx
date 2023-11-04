5d5ee45ec822165d0579705385d3d1cbd4f2c458
przepompownia <przepompownia@users.noreply.github.com>
2023-11-04 01:10:11 +0100

    nvim-treesitter-context: exclude `noice` filetype

--- lua/arctgx/pluginConfigs/nvim-treesitter-context.lua
+++ lua/arctgx/pluginConfigs/nvim-treesitter-context.lua
@@ -1,5 +1,15 @@
 require 'treesitter-context'.setup {
   enable = true,
-  throttle = true,
   max_lines = 0,
+  on_attach = function (buf)
+    local excludedFiletypes = {
+      'noice',
+    }
+
+    if vim.tbl_contains(excludedFiletypes, vim.bo[buf].filetype) then
+      return false
+    end
+
+    return true
+  end,
 }
