80194557def59e0cdf023cc958a1ef55a76823d9
przepompownia <przepompownia@users.noreply.github.com>
2023-11-17 23:27:16 +0100

    fix(keymap): don't set diagnostic mappings separately for each buffer

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -11,8 +11,14 @@ vim.api.nvim_set_hl_ns(require('arctgx.lsp').ns())
 
 local border = 'rounded'
 local origUtilOpenFloatingPreview = vim.lsp.util.open_floating_preview
+---@diagnostic disable-next-line: duplicate-set-field
 function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
   opts = opts or {}
   opts.border = opts.border or border
   return origUtilOpenFloatingPreview(contents, syntax, opts, ...)
 end
+
+vim.keymap.set('n', '<Plug>(ide-diagnostic-info)', function () vim.diagnostic.open_float({border = 'rounded'}) end, {})
+vim.keymap.set('n', '<Plug>(ide-diagnostic-goto-previous)', vim.diagnostic.goto_prev, {})
+vim.keymap.set('n', '<Plug>(ide-diagnostic-goto-next)', vim.diagnostic.goto_next, {})
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, {})
