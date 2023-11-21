72d335885b58d3eb9e0ebc30200050ab03bf579f
przepompownia <przepompownia@users.noreply.github.com>
2023-11-21 01:11:43 +0100

    Configure flatten-nvim

---
+++ lua/arctgx/pluginConfigs/flatten-nvim.lua
@@ -0,0 +1,18 @@
+require('flatten').setup({
+  callbacks = {
+    post_open = function (bufnr, _winnr, ft, _is_blocking)
+      if ft == 'gitcommit' or ft == 'gitrebase' then
+        vim.schedule(function ()
+          vim.cmd.startinsert()
+        end)
+        vim.api.nvim_create_autocmd({'QuitPre'}, {
+          buffer = bufnr,
+          once = true,
+          callback = vim.schedule_wrap(function ()
+            vim.api.nvim_buf_delete(bufnr, {})
+          end),
+        })
+      end
+    end,
+  }
+})
