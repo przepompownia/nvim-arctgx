39cf566500d99c61f663a5502418a43c5e96940b
przepompownia <przepompownia@users.noreply.github.com>
2023-11-22 00:20:37 +0100

    window.forEachWindowWithBufFileType: use vim.iter

--- lua/arctgx/window.lua
+++ lua/arctgx/window.lua
@@ -7,22 +7,12 @@ local extension = {}
 ---@param callback fun(winId: integer): any
 function extension.forEachWindowWithBufFileType(filetypes, callback)
   vim.validate({filetypes = {filetypes, {'table'}}})
-  local function runWithBuffer(winId)
-    if not api.nvim_win_is_valid(winId) then
-      return
-    end
-
-    local bufId = api.nvim_win_get_buf(winId)
-    if not vim.tbl_contains(filetypes, vim.bo[bufId].filetype) then
-      return
-    end
-
-    callback(winId)
-  end
-
-  for _, winId in ipairs(api.nvim_list_wins()) do
-    runWithBuffer(winId)
-  end
+  vim.iter(api.nvim_list_wins())
+    :filter(function (winId)
+      return api.nvim_win_is_valid(winId)
+        and vim.tbl_contains(filetypes, vim.bo[api.nvim_win_get_buf(winId)].filetype)
+    end)
+    :each(callback)
 end
 
 local function isPopup(winId)
