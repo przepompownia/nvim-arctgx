d0175b1246b0ef6ec360b204ef895df59d01aba2
przepompownia <przepompownia@users.noreply.github.com>
2023-11-22 02:00:08 +0100

    Format

--- lua/arctgx/pluginConfigs/statuscol-nvim.lua
+++ lua/arctgx/pluginConfigs/statuscol-nvim.lua
@@ -3,26 +3,26 @@ require('statuscol').setup({
   relculright = true,
   segments = {
     {
-      text = { builtin.foldfunc },
-      condition = { true },
+      text = {builtin.foldfunc},
+      condition = {true},
       click = 'v:lua.ScFa',
     },
     {
-      sign = { namespace = { 'gitsigns*' }, maxwidth = 1, colwidth = 1, auto = true },
+      sign = {namespace = {'gitsigns*'}, maxwidth = 1, colwidth = 1, auto = true},
       click = 'v:lua.ScSa',
     },
     {
-      sign = { name = { 'Diagnostic' }, maxwidth = 1, colwidth = 1, auto = true },
+      sign = {namespace = {'Diagnostic.*'}, maxwidth = 1, colwidth = 1, auto = true},
       click = 'v:lua.ScSa'
     },
     {
-      sign = { name = { 'Dap.*' }, maxwidth = 2, colwidth = 1, auto = true },
+      sign = {name = {'Dap.*'}, maxwidth = 2, colwidth = 1, auto = true},
       click = 'v:lua.ScLa'
     },
     {
-      sign = { name = { '.*' }, maxwidth = 1, colwidth = 1, auto = true },
+      sign = {name = {'.*'}, maxwidth = 1, colwidth = 1, auto = true},
       click = 'v:lua.ScSa'
     },
-    { text = { builtin.lnumfunc, ' ' }, click = 'v:lua.ScLa', },
+    {text = {builtin.lnumfunc, ' '}, click = 'v:lua.ScLa',},
   }
 })
