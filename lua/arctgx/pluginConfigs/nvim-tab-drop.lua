d027d265b40de0385a43907707857b717a1d6a4d
przepompownia <przepompownia@users.noreply.github.com>
2023-11-28 16:07:52 +0100

    Fix `nargs` for TabDrop command

--- lua/arctgx/pluginConfigs/nvim-tab-drop.lua
+++ lua/arctgx/pluginConfigs/nvim-tab-drop.lua
@@ -1,6 +1,6 @@
 local nvimTabDrop = require('nvim-tab-drop')
 local tabDrop = function (opts) nvimTabDrop(opts.args) end
-local opts = {nargs = '1', complete = 'file'}
+local opts = {nargs = 1, complete = 'file'}
 
 vim.api.nvim_create_user_command('TabDrop', tabDrop, opts)
 vim.api.nvim_create_user_command('T', tabDrop, opts)
