6f915a83a0d20c97fa55d525f53138067098d4f4
przepompownia <przepompownia@users.noreply.github.com>
2023-11-28 15:14:06 +0100

    Use fallback tabDrop implementation here

--- lua/arctgx/base.lua
+++ lua/arctgx/base.lua
@@ -2,78 +2,23 @@ local api = vim.api
 local base = {}
 local pluginDir = nil
 
-local function getBufnrByPath(path)
-  local bufnr = vim.fn.bufnr('^' .. path .. '$')
-
-  return -1 ~= bufnr and bufnr or nil
-end
-
-local function getFirstWinIdByBufnr(bufNr)
-  if nil == bufNr then
-    return nil
+local function tabDrop(path, line, column, _relativeWinId)
+  vim.cmd.drop({args = {path}, mods = {tab = #vim.api.nvim_list_tabpages()}})
+  if line then
+    vim.fn.cursor(line, column or 1)
   end
-
-  local ids = vim.fn['win_findbuf'](bufNr)
-
-  return ids[1] or nil
 end
 
-local function buffer_is_fresh(bufNr)
-  return '' == api.nvim_buf_get_name(bufNr) and api.nvim_buf_get_changedtick(bufNr) <= 2
-end
+---@type fun(path: string, line: integer?, column: integer?, relativeWinId: integer?)
+local tabDropReplacement = tabDrop
 
-local function tabDropPath(path, relativeWinId)
-  local filename = vim.fn.fnamemodify(path, ':p')
-
-  local bufNr = getBufnrByPath(filename) or vim.fn.bufadd(filename)
-  local existingWinId = getFirstWinIdByBufnr(bufNr)
-
-  if nil ~= existingWinId then
-    api.nvim_set_current_win(existingWinId)
-
-    return
-  end
-
-  relativeWinId = relativeWinId or vim.fn.win_getid()
-  local relativeBufId = api.nvim_win_get_buf(relativeWinId)
-
-  if buffer_is_fresh(relativeBufId) then
-    vim.bo[bufNr].buflisted = true
-    api.nvim_win_set_buf(relativeWinId, bufNr)
-    api.nvim_set_current_win(relativeWinId)
-    api.nvim_buf_delete(relativeBufId, {})
-
-    return
-  end
-
-  vim.cmd.tabedit(filename)
-end
-
-local function markAsPreviousPos()
-  vim.cmd.normal({bang = true, args = {"m'"}})
-end
-
-local function addCurrentPosToTagstack()
-  local curLine, curColumn = unpack(api.nvim_win_get_cursor(0))
-  local from = {api.nvim_get_current_buf(), curLine, curColumn + 1, 0}
-  local items = {{tagname = vim.fn.expand('<cword>'), from = from}}
-  vim.fn.settagstack(api.nvim_get_current_win(), {items = items}, 't')
+---@param cb fun(path: string, line: integer?, column: integer?, relativeWinId: integer?)
+function base.setTabDropReplacement(cb)
+  tabDropReplacement = cb
 end
 
 function base.tabDrop(path, line, column, relativeWinId)
-  markAsPreviousPos()
-  addCurrentPosToTagstack()
-
-  tabDropPath(path, relativeWinId)
-
-  if nil == line then
-    return
-  end
-
-  local ok, result = pcall(api.nvim_win_set_cursor, 0, {line, (column or 1) - 1})
-  if not ok then
-    vim.notify(('%s: line %s, col %s'):format(result, line, column), vim.log.levels.WARN, {title = 'tab drop'})
-  end
+  tabDropReplacement(path, line, column, relativeWinId)
 end
 
 ---comment
