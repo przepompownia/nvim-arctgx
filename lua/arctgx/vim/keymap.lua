e9bc57ab5197bed909bf8ebd86844f7c10c38c61
przepompownia <przepompownia@users.noreply.github.com>
2023-12-05 23:44:40 +0100

    keymap: remove unused code

--- lua/arctgx/vim/keymap.lua
+++ lua/arctgx/vim/keymap.lua
@@ -1,39 +1,11 @@
-local api = vim.api
-
 ---@alias KeyToPlugMapping {rhs: string, modes: table<string>, repeatable: boolean, desc: string}
 ---@alias KeyToPlugMappings table<string, KeyToPlugMapping>
 local extension = {}
 
-function extension.feedKeys(sequence)
-  api.nvim_feedkeys(api.nvim_replace_termcodes(sequence, true, false, true), 'n', false)
-end
-
----@param mode string
----@param lhs string
----@param mapping KeyToPlugMapping
----@param opts table
-local function doKeyToPlugMapping(mode, lhs, mapping, opts)
-  if mode == 'i' or true ~= mapping.repeatable then
-    vim.keymap.set({mode}, lhs, mapping.rhs, opts)
-
-    return
-  end
-
-  opts.expr = true
-  vim.keymap.set({mode}, lhs, function ()
-    require('arctgx.base').setOperatorfunc(function ()
-      local internalSeq = vim.keycode(mapping.rhs)
-      api.nvim_feedkeys(internalSeq, mode, false)
-    end)
-    return 'g@l'
-  end, opts)
-end
-
 ---@param mappings KeyToPlugMappings
 function extension.loadKeyToPlugMappings(mappings)
   for lhs, mapping in pairs(mappings) do
-    local modes = mapping.modes or {'n'}
-    for _, mode in ipairs(modes) do doKeyToPlugMapping(mode, lhs, mapping, {silent = true}) end
+    vim.keymap.set(mapping.modes, lhs, mapping.rhs, {silent = true})
   end
 end
 
