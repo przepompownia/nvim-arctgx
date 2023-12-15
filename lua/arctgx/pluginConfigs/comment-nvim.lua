7f270f893e36ad70daab51dc80041464828b584c
przepompownia <przepompownia@users.noreply.github.com>
2023-12-15 01:48:15 +0100

    comment-nvim: avoid keymaps on buffers with non-empty buftype

--- lua/arctgx/pluginConfigs/comment-nvim.lua
+++ lua/arctgx/pluginConfigs/comment-nvim.lua
@@ -16,14 +16,26 @@ local esc = vim.api.nvim_replace_termcodes(
   '<ESC>', true, false, true
 )
 
-for _, lhs in ipairs({'<C-/>', '<C-_>'}) do
-  vim.keymap.set({'n', 'i'}, lhs, function ()
-    require('Comment.api').toggle.linewise.current()
-    vim.cmd.normal 'j'
-  end)
+local function keymap(buf)
+  if vim.bo[buf].buftype ~= '' then
+    return
+  end
+  for _, lhs in ipairs({'<C-/>', '<C-_>'}) do
+    vim.keymap.set({'n', 'i'}, lhs, function ()
+      require('Comment.api').toggle.linewise.current()
+      vim.cmd.normal 'j'
+    end, {buffer = buf})
 
-  vim.keymap.set('x', lhs, function()
-    vim.api.nvim_feedkeys(esc, 'nx', false)
-    require('Comment.api').toggle.linewise(vim.fn.visualmode())
-  end)
+    vim.keymap.set('x', lhs, function ()
+      vim.api.nvim_feedkeys(esc, 'nx', false)
+      require('Comment.api').toggle.linewise(vim.fn.visualmode())
+    end, {buffer = buf})
+  end
 end
+
+vim.api.nvim_create_autocmd('BufEnter', {
+  group = vim.api.nvim_create_augroup('CommentNvimKeymaps', {clear = true}),
+  callback = function (args)
+    keymap(args.buf)
+  end
+})
