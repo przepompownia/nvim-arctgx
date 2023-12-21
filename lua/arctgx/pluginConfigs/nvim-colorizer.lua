9d9e38a2f9981705ecd34973b83d0ffda0cdf430
przepompownia <przepompownia@users.noreply.github.com>
2023-12-21 03:53:34 +0100

    nvim-colorizer: wait for termguicolors autodetection

--- lua/arctgx/pluginConfigs/nvim-colorizer.lua
+++ lua/arctgx/pluginConfigs/nvim-colorizer.lua
@@ -1,3 +1,9 @@
-if vim.go.termguicolors then
-  require 'colorizer'.setup()
-end
+vim.api.nvim_create_autocmd('OptionSet', {
+  once = true,
+  pattern = 'termguicolors',
+  callback = function ()
+    if vim.go.termguicolors then
+      require 'colorizer'.setup()
+    end
+  end,
+})
