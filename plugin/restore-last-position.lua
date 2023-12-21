89a95d0964583ff842c643e43cadb7522cb84e7c
przepompownia <przepompownia@users.noreply.github.com>
2023-12-21 02:41:18 +0100

    Format

--- plugin/restore-last-position.lua
+++ plugin/restore-last-position.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd('BufReadPost', {
-  callback = function()
+  callback = function ()
     local mark = vim.api.nvim_buf_get_mark(0, '"')
     local lines = vim.api.nvim_buf_line_count(0)
     if mark[1] > 0 and mark[1] <= lines then
