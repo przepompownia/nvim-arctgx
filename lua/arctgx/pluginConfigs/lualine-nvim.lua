2078622e49640412cfb85785ee615ba92193c6d6
przepompownia <przepompownia@users.noreply.github.com>
2023-12-21 21:18:42 +0100

    lualine-nvim: don't display tabline

--- lua/arctgx/pluginConfigs/lualine-nvim.lua
+++ lua/arctgx/pluginConfigs/lualine-nvim.lua
@@ -1,23 +1,13 @@
 local nord = require('lualine.themes.nord')
 
-local function formatFilename(name)
-  if 0 == #name then
-    return ''
-  end
-
-  name = name:gsub('[.][^.]+$', '')
-
-  return require('arctgx.string').shorten(name, 8)
-end
-
-local debugWidgetLength = 4
-
 require('lualine').setup({
   extensions = {
     'nvim-tree',
     'quickfix',
   },
   options = {
+    theme = 'nord',
+    component_separators = {left = '', right = ''},
     disabled_filetypes = {
       'fern',
       'dap-repl',
@@ -45,45 +35,23 @@ require('lualine').setup({
         'filename',
         -- modification mark
         -- fmt = formatFilename,
-      }
-    },
-  },
-  -- inactive_sections = {
-  -- },
-  tabline = {
-    lualine_a = {
+      },
       {
-        'tabs',
-        mode = 2,
-        max_length = function ()
-          return vim.o.columns - debugWidgetLength - 1
-        end,
-        tabs_color = {
-          inactive = function ()
-            if vim.opt.bg:get() == 'light' then
-              return {
-                fg = '#888888',
-                bg = '#bebebe',
-                gui = 'bold',
-              }
-            end
-            return nord.inactive.c
-          end,
-          active = function ()
-            if vim.opt.bg:get() == 'light' then
-              return {
-                fg = '#a7a7a7',
-                bg = '#ffffff',
-                gui = 'bold',
-              }
-            end
-            return nord.normal.c
-          end,
-        },
-        fmt = formatFilename,
+        'searchcount',
+        maxcount = 999,
+        timeout = 500,
+      },
+      {
+        'selectioncount',
+      },
+      {
+        'bufnr'
       },
     },
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
     lualine_z = {
+      'location',
       {
         function ()
           local out = ''
@@ -94,10 +62,13 @@ require('lualine').setup({
           end
           return out
         end,
-        max_length = debugWidgetLength,
+        color = nord.normal.c,
+        separator = {
+          left = '',
+        },
       },
-    }
-  }
+    },
+  },
 })
 
 local augroup = vim.api.nvim_create_augroup('ArctgxLualine', {clear = true})
