f5390242b278b410ae7f2452b4da86676fa4f91d
przepompownia <przepompownia@users.noreply.github.com>
2023-12-21 02:42:24 +0100

    format

--- ftplugin/php.lua
+++ ftplugin/php.lua
@@ -4,5 +4,5 @@ vim.opt_local.wrap = false
 vim.bo.softtabstop = 4
 vim.bo.shiftwidth = 4
 vim.bo.expandtab = true
-vim.keymap.set('i', '<A-.>', '->', { buffer = true })
-vim.keymap.set('i', '<A-char-62>', '=>', { buffer = true })
+vim.keymap.set('i', '<A-.>', '->', {buffer = true})
+vim.keymap.set('i', '<A-char-62>', '=>', {buffer = true})
