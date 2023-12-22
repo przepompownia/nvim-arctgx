9e427b58e9fdc58bf896b48d3e115d25c94137f2
przepompownia <przepompownia@users.noreply.github.com>
2023-12-22 18:47:17 +0100

    format

--- lua/arctgx/writebackup.lua
+++ lua/arctgx/writebackup.lua
@@ -4,7 +4,7 @@ WriteBackup.save = function ()
   local backupFile = ('%s/%s.%d'):format(
     vim.g.arctgxBackupDir or '/tmp',
     vim.fn.expand('%:t'),
-    math.random(0,9999)
+    math.random(0, 9999)
   )
 
   vim.cmd.write(backupFile)
