9386e90b45f08c2085fca2e84c87f12e59356cf5
przepompownia <przepompownia@users.noreply.github.com>
2024-01-09 20:46:26 +0100

    php: fix keymap to work in kitty

--- ftplugin/php.lua
+++ ftplugin/php.lua
@@ -5,4 +5,4 @@ vim.bo.softtabstop = 4
 vim.bo.shiftwidth = 4
 vim.bo.expandtab = true
 vim.keymap.set('i', '<A-.>', '->', {buffer = true})
-vim.keymap.set('i', '<A-char-62>', '=>', {buffer = true})
+vim.keymap.set('i', '<M-S-.>', '=>', {buffer = true})
