4c2273c9661201077e6a7a8dc4a726f3700c518a
przepompownia <przepompownia@users.noreply.github.com>
2024-01-10 01:42:35 +0100

    Stopinsert after unloading gitcommit buffer

--- ftplugin/gitcommit.lua
+++ ftplugin/gitcommit.lua
@@ -1,3 +1,9 @@
 vim.wo.cursorline = true
 vim.opt_local.number = false
 vim.cmd.startinsert()
+vim.api.nvim_create_autocmd({'BufUnload'}, {
+  buffer = vim.api.nvim_get_current_buf(),
+  callback = function ()
+    vim.cmd.stopinsert()
+  end,
+})
