bd9e04f20f3954f3ec519ce49092de976964ec55
przepompownia <przepompownia@users.noreply.github.com>
2024-02-21 21:30:34 +0100

    Load git_utils Telescope extension

--- lua/arctgx/pluginConfigs/git-utils-nvim.lua
+++ lua/arctgx/pluginConfigs/git-utils-nvim.lua
@@ -3,3 +3,5 @@ keymap.set('n', 'gitCommit', function ()
   local cwd = require('arctgx.git').top(require('arctgx.base').getBufferCwd())
   require('git-utils.commit')({gitDir = cwd})
 end)
+
+require('telescope').load_extension('git_utils')
