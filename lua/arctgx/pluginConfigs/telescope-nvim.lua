6bb6300a0e5da01f951e3654c633a3361b0b6f24
przepompownia <przepompownia@users.noreply.github.com>
2024-02-21 21:34:23 +0100

    Use gdiff from git-utils

--- lua/arctgx/pluginConfigs/telescope-nvim.lua
+++ lua/arctgx/pluginConfigs/telescope-nvim.lua
@@ -52,7 +52,7 @@ api.nvim_create_user_command(
 api.nvim_create_user_command(
   'GTDiff',
   function (opts)
-    require('arctgx.telescope.gdiff').run({
+    require('git-utils.telescope.gdiff').run({
       args = opts.fargs,
       cwd = require('arctgx.git').top(base.getBufferCwd()),
       attach_mappings = require('arctgx.telescope').defaultFileMappings,
