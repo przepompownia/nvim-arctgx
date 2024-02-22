9826e4582fc46b1eb2890ff310cc58832333d596
przepompownia <przepompownia@users.noreply.github.com>
2024-02-22 13:41:33 +0100

    Remove unused highlights

--- colors/dark.lua
+++ colors/dark.lua
@@ -1,33 +1,13 @@
 do
   local highlights = {
-    IdeFloating = {bg = '#808080'},
-    IdeDiagnosticError = {bg = '#440000'},
-    IdeDiagnosticWarning = {bg = '#442200'},
-    IdeDiagnosticInfo = {bg = '#345588'},
-    IdeDiagnosticHint = {bg = '#884499'},
-    IdeErrorFloat = {fg = '#442200'},
-    IdeHintFloat = {fg = '#cc88dd'},
-    IdeWarningFloat = {fg = '#ff8800'},
-    IdeInfoFloat = {fg = '#afffff'},
     IdeReferenceRead = {bg = '#5B532C'},
     IdeReferenceText = {bg = '#2C3C5B'},
     IdeReferenceWrite = {bg = '#4B281D'},
     IdeBreakpointSign = {fg = '#1212ff'},
-    IdeInfoSign = {fg = '#afffff'},
-    IdeWarningSign = {
-      fg = vim.api.nvim_get_hl(0, {name = 'WarningMsg'}).bg,
-    },
-    IdeHintSign = {fg = '#884499'},
-    IdeErrorSign = {fg = '#440000'},
     IdeCodeWindowCurrentFrameSign = {fg = '#440000'},
     IdeCodeWindowCurrentFrameLineNr = {
       link = 'IdeCodeWindowCurrentFrameSign',
     },
-    IdeGutterAdd = {fg = '#008800'},
-    IdeGutterChange = {fg = '#000088'},
-    IdeGutterDelete = {fg = '#880000'},
-    IdeGutterTopDelete = {fg = '#880000'},
-    IdeGutterChangeDelete = {fg = '#884400'},
   }
 
   for name, def in pairs(highlights) do
