c30d765a7d1d9cceacbf9d9ad25655d5bd09d88f
przepompownia <przepompownia@users.noreply.github.com>
2024-04-26 20:56:50 +0200

    Remove non-default keymaps for diagnostics

--- lua/arctgx/start/diagnostic-config.lua
+++ lua/arctgx/start/diagnostic-config.lua
@@ -16,7 +16,4 @@ vim.diagnostic.config({
 })
 
 local keymap = require('arctgx.vim.abstractKeymap')
-keymap.set('n', 'diagnosticOpenFloat', vim.diagnostic.open_float, {})
-keymap.set('n', 'diagnosticGotoPrevious', vim.diagnostic.goto_prev, {})
-keymap.set('n', 'diagnosticGotoNext', vim.diagnostic.goto_next, {})
 keymap.set('n', 'diagnosticSetLocList', vim.diagnostic.setloclist, {})
