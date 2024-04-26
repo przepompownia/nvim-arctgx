ed31450ea66f6cd55f451cb0d3c586f18db31565
przepompownia <przepompownia@users.noreply.github.com>
2024-04-26 15:05:02 +0200

    treesj: load on FileType

--- lua/arctgx/pluginConfigs/treesj.lua
+++ lua/arctgx/pluginConfigs/treesj.lua
@@ -1,16 +1,24 @@
-local tsj = require('treesj')
-local keymap = require('arctgx.vim.abstractKeymap')
+vim.api.nvim_create_autocmd('FileType', {
+  once = true,
+  callback = function (args)
+    if args.match == 'noice' then
+      return
+    end
+    local tsj = require('treesj')
+    local keymap = require('arctgx.vim.abstractKeymap')
 
-local langs = {}
+    local langs = {}
 
-tsj.setup({
-  use_default_keymaps = false,
-  check_syntax_error = true,
-  max_join_length = 120,
-  cursor_behavior = 'hold',
-  notify = true,
-  langs = langs,
+    tsj.setup({
+      use_default_keymaps = false,
+      check_syntax_error = true,
+      max_join_length = 512,
+      cursor_behavior = 'hold',
+      notify = true,
+      langs = langs,
+    })
+    keymap.set({'n'}, 'toggleSplitJoinLinesAtCursor', tsj.toggle, {})
+    keymap.set({'n'}, 'splitLinesAtCursor', tsj.split, {})
+    keymap.set({'n'}, 'joinLinesAtCursor', tsj.join, {})
+  end
 })
-keymap.set({'n'}, 'toggleSplitJoinLinesAtCursor', tsj.toggle, {})
-keymap.set({'n'}, 'splitLinesAtCursor', tsj.split, {})
-keymap.set({'n'}, 'joinLinesAtCursor', tsj.join, {})
