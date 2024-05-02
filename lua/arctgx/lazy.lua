e8a6bc769314f12f947cfdc56e5e6ce98b84d38c
przepompownia <przepompownia@users.noreply.github.com>
2024-05-02 23:38:05 +0200

    lazy: allow add multiple configuration per module

--- lua/arctgx/lazy.lua
+++ lua/arctgx/lazy.lua
@@ -1,6 +1,6 @@
 local lazy = {}
 
---- @type table <string, table>
+--- @type table <string, {method: string, args: table}[]>
 local configs = {}
 --- @type table <string, boolean>
 local loaded = {}
@@ -9,21 +9,34 @@ function lazy.setupAtFirstLoad(modname)
   if configs[modname] and not loaded[modname] then
     loaded[modname] = true
     local module = require(modname)
-    if type(module) == 'table' and type(rawget(module, 'setup')) == 'function' then
-      module.setup(configs[modname])
-      return function ()
-        return module
+
+    for _, config in ipairs(configs[modname]) do
+      if type(module) == 'table' and type(module[config.method]) == 'function' then
+        module[config.method](unpack(config.args))
       end
+    end
+
+    return function ()
+      return module
     end
   end
 end
 
 setmetatable(lazy, {
   __index = function (_, modname)
-    return {setup = function (config)
-      configs[modname] = config
-    end}
+    return setmetatable({}, {
+      __index = function (_, method)
+        return function (...)
+          configs[modname] = configs[modname] or {}
+          configs[modname][#configs[modname] + 1] = {method = method, args = {...}}
+        end
+      end
+    })
   end
 })
+
+function lazy.printConfigs()
+  dump(configs)
+end
 
 return lazy
