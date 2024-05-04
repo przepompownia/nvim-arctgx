c3a79eb87f95bdb08338c34cf2d6ec4408e896ef
przepompownia <przepompownia@users.noreply.github.com>
2024-05-04 22:27:55 +0200

    nvim-dap-virtual-text: lazy load (on `dap`)

--- lua/arctgx/pluginConfigs/nvim-dap-virtual-text.lua
+++ lua/arctgx/pluginConfigs/nvim-dap-virtual-text.lua
@@ -1,44 +1,39 @@
-local api = vim.api
--- local dap = require('dap')
-local virtualText = require('nvim-dap-virtual-text')
-
---- @param variable any
---- @param buf any
---- @return boolean
-local function skipDisplayingVariable(variable, buf)
-  local filetype = vim.bo[buf].filetype
-  if variable.name == '$this' and filetype == 'php' then
-    return true
-  end
-  return false
-end
-
-virtualText.setup {
-  enabled = true,
-  enabled_commands = true,
-  highlight_changed_variables = true,
-  highlight_new_as_changed = false,
-  show_stop_reason = true,
-  commented = false,
-  only_first_definition = false,
-  all_references = true,
-  filter_references_pattern = '<module',
-  -- virt_text_pos = 'eol',
-  all_frames = false,
-  virt_lines = false,
-  virt_text_win_col = nil,
-  display_callback = function (variable, bufId, _stackframe, _node)
-    if true == skipDisplayingVariable(variable, bufId) then
-      return ''
+require('arctgx.lazy').setupOnLoad('dap', function ()
+  local function skipDisplayingVariable(variable, buf)
+    local filetype = vim.bo[buf].filetype
+    if variable.name == '$this' and filetype == 'php' then
+      return true
     end
-    -- dap.session():evaluate()
+    return false
+  end
 
-    return variable.name .. ': ' .. variable.value
-  end,
-}
+  require('nvim-dap-virtual-text').setup {
+    enabled = true,
+    enabled_commands = true,
+    highlight_changed_variables = true,
+    highlight_new_as_changed = false,
+    show_stop_reason = true,
+    commented = false,
+    only_first_definition = false,
+    all_references = true,
+    filter_references_pattern = '<module',
+    -- virt_text_pos = 'eol',
+    all_frames = false,
+    virt_lines = false,
+    virt_text_win_col = nil,
+    display_callback = function (variable, bufId, _stackframe, _node)
+      if true == skipDisplayingVariable(variable, bufId) then
+        return ''
+      end
+      -- dap.session():evaluate()
 
-local augroup = api.nvim_create_augroup('ArctgxDapVirtualText', {clear = true})
-api.nvim_create_autocmd('User', {
+      return variable.name .. ': ' .. variable.value
+    end,
+  }
+end)
+
+local augroup = vim.api.nvim_create_augroup('ArctgxDapVirtualText', {clear = true})
+vim.api.nvim_create_autocmd('User', {
   group = augroup,
   pattern = 'DAPClean',
   callback = require('nvim-dap-virtual-text.virtual_text').clear_virtual_text,
