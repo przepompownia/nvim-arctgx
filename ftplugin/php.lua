2b1faa03fdd68d464d12cd9ff0b624a09bae00f8
przepompownia <przepompownia@users.noreply.github.com>
2024-05-04 19:01:41 +0200

    ftplugin: php: disable built-in ftplugin, set commentstring

--- ftplugin/php.lua
+++ ftplugin/php.lua
@@ -1,6 +1,9 @@
 vim.opt_local.iskeyword:remove('$')
 vim.opt_local.spell = true
 vim.opt_local.wrap = false
+vim.opt_local.commentstring = '// %s'
+vim.opt_local.comments = 's1:/*,mb:*,ex:*/,://,:#'
+vim.b.did_ftplugin = 1
 vim.bo.softtabstop = 4
 vim.bo.shiftwidth = 4
 vim.bo.expandtab = true
