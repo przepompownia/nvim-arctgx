eda13ddfaa97d9baffa80f7d4a4703bd52d6abf4
przepompownia <przepompownia@users.noreply.github.com>
2024-05-04 18:54:42 +0200

    nvim-surround: add lua inline function wrapper

--- lua/arctgx/pluginConfigs/nvim-surround.lua
+++ lua/arctgx/pluginConfigs/nvim-surround.lua
@@ -1,1 +1,12 @@
-require("nvim-surround").setup({})
+require('nvim-surround').setup({
+  surrounds = {
+    f = {
+      add = function ()
+        if vim.bo.filetype ~= 'lua' then
+          return
+        end
+        return {{'function () '}, {'() end'}}
+      end
+    }
+  }
+})
