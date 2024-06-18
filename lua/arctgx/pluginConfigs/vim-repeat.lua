7a7c711734dbb187c23c6e3879ece30d6be1c108
przepompownia <przepompownia@users.noreply.github.com>
2024-06-18 14:38:47 +0200

    Remove unused config

--- lua/arctgx/pluginConfigs/vim-repeat.lua
+++
@@ -1,7 +0,0 @@
-local augroup = vim.api.nvim_create_augroup('repeatPluginExtension', {clear = true})
-vim.api.nvim_create_autocmd({'BufEnter', 'BufWritePost'}, {
-  group = augroup,
-  callback = function()
-    vim.g.repeat_tick = vim.b.changedtick
-  end,
-})
