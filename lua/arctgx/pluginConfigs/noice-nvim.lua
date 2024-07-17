4d116b2dcce5dc64b78242c36cb5270d0959e947
przepompownia <przepompownia@users.noreply.github.com>
2024-07-17 21:32:46 +0200

    Defer setting up Noice again
    Start-up messages from missing bash-language-server revealed failed
    assertion.

--- lua/arctgx/pluginConfigs/noice-nvim.lua
+++ lua/arctgx/pluginConfigs/noice-nvim.lua
@@ -1,53 +1,60 @@
-require('noice').setup({
-  messages = {
-    view = 'mini',
-    view_search = false,
-    view_error = 'mini',
-    view_warn = 'mini',
-  },
-  notify = {
-    enabled = true,
-    view = 'mini',
-  },
-  lsp = {
-    override = {
-      -- ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-      -- ["vim.lsp.util.stylize_markdown"] = true,
-      -- ["cmp.entry.get_documentation"] = true,
+local function setupNoice()
+  require('noice').setup({
+    messages = {
+      view = 'mini',
+      view_search = false,
+      view_error = 'mini',
+      view_warn = 'mini',
     },
-    signature = {
+    notify = {
       enabled = true,
-      auto_open = {
-        enabled = true,
-        trigger = true,
-        luasnip = true,
-        throttle = 50,
+      view = 'mini',
+    },
+    lsp = {
+      override = {
+        -- ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        -- ["vim.lsp.util.stylize_markdown"] = true,
+        -- ["cmp.entry.get_documentation"] = true,
       },
-      view = nil, -- {
-      ---@type NoiceViewOptions
-      -- opts = {
-      --   win_options = { border = 'rounded' },
-      -- },
+      signature = {
+        enabled = true,
+        auto_open = {
+          enabled = true,
+          trigger = true,
+          luasnip = true,
+          throttle = 50,
+        },
+        view = nil, -- {
+        ---@type NoiceViewOptions
+        -- opts = {
+        --   win_options = { border = 'rounded' },
+        -- },
+      },
+      hover = {
+        enabled = false,
+        view = nil,
+        ---@type NoiceViewOptions
+        opts = {},
+      },
     },
-    hover = {
-      enabled = false,
-      view = nil,
-      ---@type NoiceViewOptions
-      opts = {},
+    views = {
+      cmdline_input = {
+        relative = 'editor',
+      },
+      mini = {
+        timeout = 5000,
+        reverse = false,
+      },
     },
-  },
-  views = {
-    cmdline_input = {
-      relative = 'editor',
+    presets = {
+      lsp_doc_border = true,
     },
-    mini = {
-      timeout = 5000,
-      reverse = false,
-    },
-  },
-  presets = {
-    lsp_doc_border = true,
-  },
+  })
+end
+
+vim.api.nvim_create_autocmd('UIEnter', {
+  once = true,
+  callback = setupNoice,
 })
 
 vim.keymap.set('c', '<A-CR>', function ()
