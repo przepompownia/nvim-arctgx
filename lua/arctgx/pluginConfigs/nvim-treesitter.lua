30cc723165c0ab85ccf85724bba31a5fb57205b6
przepompownia <przepompownia@users.noreply.github.com>
2024-07-19 22:04:38 +0200

    nvim-treesitter: support both master and main branch

--- lua/arctgx/pluginConfigs/nvim-treesitter.lua
+++ lua/arctgx/pluginConfigs/nvim-treesitter.lua
@@ -37,34 +37,79 @@ local additionalParsers = {
   'yaml',
 }
 
-local keymap = require('arctgx.vim.abstractKeymap')
-local tsKeymaps = {
-  init_selection = keymap.firstLhs('langIncrementSelection'),
-  node_incremental = keymap.firstLhs('langIncrementSelection'),
-  node_decremental = keymap.firstLhs('langDecrementSelection'),
-  scope_incremental = keymap.firstLhs('langScopeIncrementSelection'),
-}
-
-require 'nvim-treesitter.configs'.setup {
-  ensure_installed = additionalParsers,
-  highlight = {
-    enable = true,
-  },
-  incremental_selection = {
-    enable = true,
-    keymaps = tsKeymaps,
-  },
-  indent = {
-    enable = true,
-  },
-}
-
-local tsLangMap = {
+local ftLangMap = {
   php = 'php_only',
 }
 
-for filetype, lang in pairs(tsLangMap) do
+local notFtiletype = {
+  php_only = true,
+}
+
+for _, parser in ipairs(additionalParsers) do
+  if nil == notFtiletype[parser] then
+    ftLangMap[parser] = parser
+  end
+end
+
+local function configureMaster()
+  local keymap = require('arctgx.vim.abstractKeymap')
+  local tsKeymaps = {
+    init_selection = keymap.firstLhs('langIncrementSelection'),
+    node_incremental = keymap.firstLhs('langIncrementSelection'),
+    node_decremental = keymap.firstLhs('langDecrementSelection'),
+    scope_incremental = keymap.firstLhs('langScopeIncrementSelection'),
+  }
+
+  require 'nvim-treesitter.configs'.setup {
+    ensure_installed = additionalParsers,
+    highlight = {
+      enable = true,
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = tsKeymaps,
+    },
+    indent = {
+      enable = true,
+    },
+  }
+end
+
+for filetype, lang in pairs(ftLangMap) do
   if filetype ~= lang then
     vim.treesitter.language.register(lang, filetype)
   end
 end
+
+local function configureMain()
+  require('nvim-treesitter').setup()
+
+  require('nvim-treesitter.install').install(vim.tbl_values(ftLangMap), {skip = {installed = true}}, function ()
+    vim.api.nvim_exec_autocmds('User', {pattern = 'TSInstallFinished'})
+  end)
+
+  vim.api.nvim_create_autocmd('FileType', {
+    pattern = vim.tbl_keys(ftLangMap),
+    callback = function (event)
+      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      local ok, error = pcall(vim.treesitter.start, event.buf, ftLangMap[event.match])
+      if not ok and error:find('no parser') then
+        vim.api.nvim_create_autocmd('User', {
+          buffer = event.buf,
+          callback = function (tsinstallEvent)
+            if not tsinstallEvent.match == 'TSInstallFinished' then
+              return
+            end
+            vim.treesitter.start(event.buf, ftLangMap[event.match])
+          end
+        })
+      end
+    end
+  })
+end
+
+if require('nvim-treesitter.install').install then
+  configureMain()
+  return
+end
+configureMaster()
