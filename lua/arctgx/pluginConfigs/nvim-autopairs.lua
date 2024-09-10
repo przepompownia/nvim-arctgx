78cc7c91946e12845bb8419f2bb3d80d68b244e8
przepompownia <przepompownia@users.noreply.github.com>
2024-09-10 14:40:27 +0200

    nvim-autopairs: remove dependency check

--- lua/arctgx/pluginConfigs/nvim-autopairs.lua
+++ lua/arctgx/pluginConfigs/nvim-autopairs.lua
@@ -1,7 +1,6 @@
 local autopairs = require('nvim-autopairs')
 local Rule = require('nvim-autopairs.rule')
 local okCmp, cmp = pcall(require, 'cmp')
-local okTs, _ = pcall(require, 'nvim-treesitter.parsers')
 
 autopairs.setup {
   check_ts = true,
@@ -19,22 +18,20 @@ for start, _ in pairs(existingRules) do
   autopairs.remove_rule(start)
 end
 
-if okTs then
-  local _, cond = pcall(require, 'nvim-autopairs.conds')
-  local function isAfterTypeInPhpDocblock()
-    local captures = require('arctgx.treesitter').getCapturesBeforeCursor(0)
-    if not vim.tbl_contains(captures, 'comment') then
-      return false
-    end
-
-    return vim.tbl_contains(captures, 'type')
+local _, cond = pcall(require, 'nvim-autopairs.conds')
+local function isAfterTypeInPhpDocblock()
+  local captures = require('arctgx.treesitter').getCapturesBeforeCursor(0)
+  if not vim.tbl_contains(captures, 'comment') then
+    return false
   end
 
-  autopairs.add_rules({
-    existingRules['`']:with_pair(cond.not_filetypes({'sql', 'mysql'})),
-    Rule('<', '>', 'php'):with_pair(isAfterTypeInPhpDocblock),
-  })
+  return vim.tbl_contains(captures, 'type')
 end
+
+autopairs.add_rules({
+  existingRules['`']:with_pair(cond.not_filetypes({'sql', 'mysql'})),
+  Rule('<', '>', 'php'):with_pair(isAfterTypeInPhpDocblock),
+})
 
 if okCmp then
   local cmp_autopairs = require('nvim-autopairs.completion.cmp')
