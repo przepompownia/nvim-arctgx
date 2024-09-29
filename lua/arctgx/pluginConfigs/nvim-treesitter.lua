a3881a0f91af30a16733b10645cfb08534c34d00
przepompownia <przepompownia@users.noreply.github.com>
2024-09-29 20:04:14 +0200

    treesitter: parsers can be loaded independently of nvim-treesitter
    existence.

--- lua/arctgx/pluginConfigs/nvim-treesitter.lua
+++ lua/arctgx/pluginConfigs/nvim-treesitter.lua
@@ -1,71 +1,4 @@
-local fileTypes = {
-  'awk',
-  'bash',
-  'css',
-  'diff',
-  'dockerfile',
-  'editorconfig',
-  'gitattributes',
-  'gitcommit',
-  'gitignore',
-  'git_rebase',
-  'git_config',
-  'html',
-  'http',
-  'ini',
-  'javascript',
-  'jq',
-  'json',
-  'json5',
-  'jsonc',
-  'make',
-  'muttrc',
-  'passwd',
-  'python',
-  'regex',
-  'sql',
-  'strace',
-  'ssh_config',
-  'tmux',
-  'todotxt',
-  'toml',
-  'udev',
-  'twig',
-  'xml',
-  'yaml',
-}
-
-local notFtLangs = {
-  'luadoc',
-  'phpdoc',
-  'php_only',
-}
-
-local additionalFiletypes = {
-  'php',
-  'markdown',
-  'markdown_inline',
-  'c',
-  'vim',
-}
-
-local ftLangMap = {
-  php = 'php_only',
-}
-
-local langs = vim.deepcopy(fileTypes)
-
-for _, lang in ipairs(notFtLangs) do
-  langs[#langs + 1] = lang
-end
-
-for _, fileType in ipairs(additionalFiletypes) do
-  fileTypes[#fileTypes + 1] = fileType
-end
-
-for filetype, lang in pairs(ftLangMap) do
-  vim.treesitter.language.register(lang, filetype)
-end
+local langs = require('arctgx.treesitter').langs()
 
 local function configureMain()
   require('nvim-treesitter').setup({})
@@ -77,33 +10,6 @@ local function configureMain()
       vim.api.nvim_exec_autocmds('User', {pattern = 'TSInstallFinished'})
     end
   )
-
-  vim.api.nvim_create_autocmd('FileType', {
-    pattern = fileTypes,
-    callback = function (event)
-      local function start(buf, lang)
-        vim.treesitter.start(buf, lang)
-        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-      end
-
-      local lang = vim.treesitter.language.get_lang(event.match)
-
-      if vim.treesitter.language.add(lang) then
-        start(event.buf, lang)
-        return
-      end
-
-      vim.api.nvim_create_autocmd('User', {
-        once = true,
-        buffer = event.buf,
-        callback = function (tsinstallEvent)
-          if tsinstallEvent.match == 'TSInstallFinished' then
-            start(event.buf, lang)
-          end
-        end
-      })
-    end
-  })
 end
 
 local function configureMaster()
--- lua/arctgx/treesitter.lua
+++ lua/arctgx/treesitter.lua
@@ -1,5 +1,105 @@
 local extension = {}
 
+local fileTypes = {
+  'awk',
+  'bash',
+  'css',
+  'diff',
+  'dockerfile',
+  'editorconfig',
+  'gitattributes',
+  'gitcommit',
+  'gitignore',
+  'git_rebase',
+  'git_config',
+  'html',
+  'http',
+  'ini',
+  'javascript',
+  'jq',
+  'json',
+  'json5',
+  'jsonc',
+  'make',
+  'muttrc',
+  'passwd',
+  'python',
+  'regex',
+  'sql',
+  'strace',
+  'ssh_config',
+  'tmux',
+  'todotxt',
+  'toml',
+  'udev',
+  'twig',
+  'xml',
+  'yaml',
+}
+
+local notFtLangs = {
+  'luadoc',
+  'phpdoc',
+  'php_only',
+}
+
+local additionalFiletypes = {
+  'php',
+  'markdown',
+  'markdown_inline',
+  'c',
+  'vim',
+}
+
+local ftLangMap = {
+  php = 'php_only',
+}
+
+local langs = vim.deepcopy(fileTypes)
+
+for _, lang in ipairs(notFtLangs) do
+  langs[#langs + 1] = lang
+end
+
+for _, fileType in ipairs(additionalFiletypes) do
+  fileTypes[#fileTypes + 1] = fileType
+end
+
+for filetype, lang in pairs(ftLangMap) do
+  vim.treesitter.language.register(lang, filetype)
+end
+
+function extension.langs()
+  return langs
+end
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = fileTypes,
+  callback = function (event)
+    local function start(buf, lang)
+      vim.treesitter.start(buf, lang)
+      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end
+
+    local lang = vim.treesitter.language.get_lang(event.match)
+
+    if vim.treesitter.language.add(lang) then
+      start(event.buf, lang)
+      return
+    end
+
+    vim.api.nvim_create_autocmd('User', {
+      once = true,
+      buffer = event.buf,
+      callback = function (tsinstallEvent)
+        if tsinstallEvent.match == 'TSInstallFinished' then
+          start(event.buf, lang)
+        end
+      end
+    })
+  end
+})
+
 --- from https://github.com/neovim/neovim/blob/95fd1ad83e24bbb14cc084fb001251939de6c0a9/runtime/lua/vim/treesitter.lua#L257
 function extension.getCapturesBeforeCursor(winnr)
   winnr = winnr or 0
