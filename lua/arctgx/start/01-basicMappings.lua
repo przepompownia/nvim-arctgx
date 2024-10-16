db6ec8340650cf6c3f76824c501d9b3246275085
przepompownia <przepompownia@users.noreply.github.com>
2024-10-17 01:30:31 +0200

    Localize vim.keymap

--- lua/arctgx/start/01-basicMappings.lua
+++ lua/arctgx/start/01-basicMappings.lua
@@ -1,48 +1,49 @@
+local keymap = vim.keymap
 local api = vim.api
-
 local base = require 'arctgx.base'
 
 local opts = {silent = true}
-vim.keymap.set({'n'}, '<Leader>=', 'ggVG=', opts)
-vim.keymap.set({'n'}, '<Leader>/', '/', {})
-vim.keymap.set({'n'}, '<Leader>?', '?', {})
-vim.keymap.set({'n'}, '/', '/\\c', {})
-vim.keymap.set({'n'}, '?', '?\\c', {})
-vim.keymap.set({'i'}, '<C-q>', '<C-r>+', opts)
-vim.keymap.set({'x'}, '<C-Insert>', '"*y', opts)
-vim.keymap.set({'x'}, '<C-S-Insert>', '"+y', opts)
-vim.keymap.set({'!'}, '<S-Insert>', '<MiddleMouse>', opts)
-vim.keymap.set({'n', 'v'}, '<Tab>', '<C-w>w', opts)
-vim.keymap.set({'n'}, 'g<Left>', '<C-O>', opts)
-vim.keymap.set({'n'}, 'g<Right>', '<C-I>', opts)
-vim.keymap.set({'i', 'c'}, '<Insert>', '<Nop>', opts)
-vim.keymap.set({'i'}, '<C-z>', '<C-x><C-o>', opts)
-vim.keymap.set({'n'}, 'Q', '<Nop>', opts)
-vim.keymap.set({'c'}, '<C-BS>', '<C-w>', opts)
-vim.keymap.set({'c'}, '<C-Del>', '<S-Right><C-w>', opts)
-vim.keymap.set({'n'}, '<S-Up>', '<C-y>', opts)
-vim.keymap.set({'n'}, '<S-Down>', '<C-e>', opts)
-vim.keymap.set({'n'}, '<C-w>C', vim.cmd.tabclose, opts)
-vim.keymap.set({'n'}, '<C-;>', ':lua ', {})
+keymap.set({'n'}, '<Leader>=', 'ggVG=', opts)
+keymap.set({'n'}, '<Leader>/', '/', {})
+keymap.set({'n'}, '<Leader>?', '?', {})
+keymap.set({'n'}, '/', '/\\c', {})
+keymap.set({'n'}, '?', '?\\c', {})
+keymap.set({'i'}, '<C-q>', '<C-r>+', opts)
+keymap.set({'x'}, '<C-Insert>', '"*y', opts)
+keymap.set({'x'}, '<C-S-Insert>', '"+y', opts)
+keymap.set({'!'}, '<S-Insert>', '<MiddleMouse>', opts)
+keymap.set({'n', 'v'}, '<Tab>', '<C-w>w', opts)
+keymap.set({'n'}, 'g<Left>', '<C-O>', opts)
+keymap.set({'n'}, 'g<Right>', '<C-I>', opts)
+keymap.set({'i', 'c'}, '<Insert>', '<Nop>', opts)
+keymap.set({'i'}, '<C-z>', '<C-x><C-o>', opts)
+keymap.set({'n'}, 'Q', '<Nop>', opts)
+keymap.set({'c'}, '<C-BS>', '<C-w>', opts)
+keymap.set({'c'}, '<C-Del>', '<S-Right><C-w>', opts)
+keymap.set({'n'}, '<S-Up>', '<C-y>', opts)
+keymap.set({'n'}, '<S-Down>', '<C-e>', opts)
+keymap.set({'n'}, '<C-w>C', vim.cmd.tabclose, opts)
+keymap.set({'n'}, '<C-;>', ':lua ', {})
 
 local function selectionFromPastedRange()
   return '`[' .. vim.fn.getregtype():sub(0, 1) .. '`]'
 end
 
-vim.keymap.set('n', 'gp', selectionFromPastedRange, {expr = true, silent = true})
-vim.keymap.set('v', ']p', 'pgv=', {silent = true, desc = 'Paste with indentation'})
+keymap.set('n', 'gp', selectionFromPastedRange, {expr = true, silent = true})
+keymap.set('v', ']p', 'pgv=', {silent = true, desc = 'Paste with indentation'})
 
-vim.keymap.set('n', ']p', function ()
+keymap.set('n', ']p', function ()
   return 'p' .. selectionFromPastedRange() .. '='
 end, {expr = true, desc = 'Paste with indentation', silent = true})
 
-vim.keymap.set('n', '<A-j>', function ()
+keymap.set('n', '<A-j>', function ()
   if vim.fn.line('.') == vim.fn.line('$') then
     return
   end
   return 'ddp' .. selectionFromPastedRange() .. '='
 end, {expr = true, desc = 'Move single line down with indentation', silent = true})
-vim.keymap.set('v', '<A-j>', function ()
+
+keymap.set('v', '<A-j>', function ()
   local max = vim.fn.getpos('.')[2]
   local vline = vim.fn.getpos('v')[2]
   if vline > max then
@@ -54,14 +55,14 @@ vim.keymap.set('v', '<A-j>', function ()
   return 'dp' .. selectionFromPastedRange() .. '=gv'
 end, {expr = true, desc = 'Move single line down with indentation', silent = true})
 
-vim.keymap.set('n', '<A-k>', function ()
+keymap.set('n', '<A-k>', function ()
   if api.nvim_win_get_cursor(0)[1] == 1 then
     return
   end
   return 'ddkP' .. selectionFromPastedRange() .. '='
 end, {expr = true, desc = 'Move single line up with indentation', silent = true})
 
-vim.keymap.set('v', '<A-k>', function ()
+keymap.set('v', '<A-k>', function ()
   local min = vim.fn.getpos('.')[2]
   local vline = vim.fn.getpos('v')[2]
   if vline < min then
@@ -78,13 +79,13 @@ local function setreg(register, value)
   vim.notify(('"%s" copied to reg "%s"'):format(value, register), vim.log.levels.INFO)
 end
 
-vim.keymap.set('t', '<S-Insert>', '<C-\\><C-N>"*pi', opts)
-vim.keymap.set('n', 'i', function () base.insertWithInitialIndentation('i') end, opts)
-vim.keymap.set('n', 'a', function () base.insertWithInitialIndentation('a') end, opts)
-vim.keymap.set('n', '<Leader>fcc', function () setreg('+', vim.fn.expand('%:.')) end, opts)
-vim.keymap.set('n', '<Leader>fcC', function () setreg('+', vim.fn.expand('%:p')) end, opts)
-vim.keymap.set('n', '<Leader>co', vim.cmd.copen, opts)
-vim.keymap.set('t', '<M-p>', function ()
+keymap.set('t', '<S-Insert>', '<C-\\><C-N>"*pi', opts)
+keymap.set('n', 'i', function () base.insertWithInitialIndentation('i') end, opts)
+keymap.set('n', 'a', function () base.insertWithInitialIndentation('a') end, opts)
+keymap.set('n', '<Leader>fcc', function () setreg('+', vim.fn.expand('%:.')) end, opts)
+keymap.set('n', '<Leader>fcC', function () setreg('+', vim.fn.expand('%:p')) end, opts)
+keymap.set('n', '<Leader>co', vim.cmd.copen, opts)
+keymap.set('t', '<M-p>', function ()
   local regname = vim.fn.nr2char(vim.fn.getchar())
   local sequence = ('<C-\\><C-n>"%spi'):format(regname)
   return api.nvim_replace_termcodes(sequence, true, false, true)
@@ -93,28 +94,28 @@ end, {
   expr = true,
 })
 
-vim.keymap.set({'i', 'n'}, '<A-Up>', function () vim.cmd.wincmd('k') end, opts)
-vim.keymap.set({'i', 'n'}, '<A-Down>', function () vim.cmd.wincmd('j') end, opts)
-vim.keymap.set({'i', 'n'}, '<A-Left>', function () vim.cmd.wincmd('h') end, opts)
-vim.keymap.set({'i', 'n'}, '<A-Right>', function () vim.cmd.wincmd('l') end, opts)
-vim.keymap.set('i', '<C-BS>', '<C-w>', opts)
-vim.keymap.set('i', '<C-Del>', function () vim.cmd.normal('dw') end, opts)
-vim.keymap.set({'i', 'n', 'x'}, '<F2>', vim.cmd.update, opts)
-vim.keymap.set({'i', 'n', 'x'}, '<F3>', vim.cmd.quit, opts)
-vim.keymap.set({'i', 'n'}, '<S-F3>', function () vim.cmd.quit {bang = true} end, opts)
-vim.keymap.set({'i', 'n'}, '<F15>', function () vim.cmd.quit {bang = true} end, opts)
-vim.keymap.set({'i'}, '<C-Left>', function () vim.cmd.normal('b') end, opts)
-vim.keymap.set({'i'}, '<C-Right>', function () vim.cmd.normal('w') end, opts)
-vim.keymap.set({'i'}, '<S-Left>', function () vim.cmd.normal('v') end, opts)
-vim.keymap.set({'i'}, '<S-Right>', function () vim.cmd.normal('v') end, opts)
-vim.keymap.set('n', '<Esc>', vim.cmd.fclose, {desc = 'Close float window'})
+keymap.set({'i', 'n'}, '<A-Up>', function () vim.cmd.wincmd('k') end, opts)
+keymap.set({'i', 'n'}, '<A-Down>', function () vim.cmd.wincmd('j') end, opts)
+keymap.set({'i', 'n'}, '<A-Left>', function () vim.cmd.wincmd('h') end, opts)
+keymap.set({'i', 'n'}, '<A-Right>', function () vim.cmd.wincmd('l') end, opts)
+keymap.set('i', '<C-BS>', '<C-w>', opts)
+keymap.set('i', '<C-Del>', function () vim.cmd.normal('dw') end, opts)
+keymap.set({'i', 'n', 'x'}, '<F2>', vim.cmd.update, opts)
+keymap.set({'i', 'n', 'x'}, '<F3>', vim.cmd.quit, opts)
+keymap.set({'i', 'n'}, '<S-F3>', function () vim.cmd.quit {bang = true} end, opts)
+keymap.set({'i', 'n'}, '<F15>', function () vim.cmd.quit {bang = true} end, opts)
+keymap.set({'i'}, '<C-Left>', function () vim.cmd.normal('b') end, opts)
+keymap.set({'i'}, '<C-Right>', function () vim.cmd.normal('w') end, opts)
+keymap.set({'i'}, '<S-Left>', function () vim.cmd.normal('v') end, opts)
+keymap.set({'i'}, '<S-Right>', function () vim.cmd.normal('v') end, opts)
+keymap.set('n', '<Esc>', vim.cmd.fclose, {desc = 'Close float window'})
 
 for _, lhs in ipairs({'<C-/>', '<C-_>'}) do
-  vim.keymap.set({'n', 'i'}, lhs, function ()
+  keymap.set({'n', 'i'}, lhs, function ()
     vim.cmd.normal 'gccj'
   end)
 
-  vim.keymap.set('x', lhs, function ()
+  keymap.set('x', lhs, function ()
     vim.cmd.normal 'gc'
   end)
 end
