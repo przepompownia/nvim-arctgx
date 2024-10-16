550ed8bf26f30e2bc9b3aef8c0fbfb3aacd86e53
przepompownia <przepompownia@users.noreply.github.com>
2024-10-16 16:04:31 +0200

    man: `q` become mapped by default

--- after/ftplugin/man.lua
+++ after/ftplugin/man.lua
@@ -4,5 +4,5 @@ if #api.nvim_tabpage_list_wins(0) > 1 then
 end
 vim.bo.modified = false
 vim.bo.modifiable = false
-vim.keymap.set({'n', 'v'}, 'q', vim.cmd.quit, {buffer = api.nvim_get_current_buf()})
+vim.keymap.set({'v'}, 'q', vim.cmd.quit, {buffer = api.nvim_get_current_buf()})
 vim.keymap.set({'n'}, '<Space>', '<C-f>', {buffer = api.nvim_get_current_buf()})
