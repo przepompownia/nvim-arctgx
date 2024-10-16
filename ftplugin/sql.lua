96118aa395d19424edf7ada6c80407e73ced1ba1
przepompownia <przepompownia@users.noreply.github.com>
2024-10-16 16:06:40 +0200

    sql: remap `update` to `write`

--- ftplugin/sql.lua
+++ ftplugin/sql.lua
@@ -5,3 +5,4 @@ vim.bo.expandtab = true
 vim.g.loaded_sql_completion = 1
 vim.g.omni_sql_no_default_maps = 1
 vim.g.ftplugin_sql_omni_key = '<Nop>'
+vim.keymap.set({'n', 'i', 'v'}, '<F2>', vim.cmd.write, {buffer = 0, desc = 'Write SQL query'})
