9661c93fe59b8d1478ad5979cb170ac71cd082e3
przepompownia <przepompownia@users.noreply.github.com>
2024-11-08 02:01:35 +0100

    Configure nvim-snippets

---
+++ lua/arctgx/pluginConfigs/nvim-snippets.lua
@@ -0,0 +1,9 @@
+require('snippets').setup({
+  global_snippets = nil,
+  create_autocmd = true,
+  create_cmp_source = false,
+  allowed_filetypes = {'lua', 'php'},
+  search_paths = {
+    '/home/arctgx/.vim/nvim/pack/arctgx/opt/arctgx/snippets'
+  },
+})
