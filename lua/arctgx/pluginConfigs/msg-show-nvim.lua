3309c4c55186550cd1526114cf91ad27dc0c1a73
przepompownia <przepompownia@users.noreply.github.com>
2024-12-20 02:25:53 +0100

    Don't override `vim.notify`

--- lua/arctgx/pluginConfigs/msg-show-nvim.lua
+++ lua/arctgx/pluginConfigs/msg-show-nvim.lua
@@ -2,5 +2,4 @@ local notifier = require('msg-show.notifier')
 notifier.setup()
 
 require('msg-show.redir').init(notifier.addUiMessage, notifier.updateUiMessage, notifier.debug)
-vim.notify = notifier.notify
 require('arctgx.vim.abstractKeymap').set('n', 'browseNotificationHistory', notifier.showHistory)
