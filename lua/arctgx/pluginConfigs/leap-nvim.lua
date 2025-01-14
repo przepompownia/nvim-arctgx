fddd2700c0c1bbebdc942c54ed26afafc2d62598
przepompownia <przepompownia@users.noreply.github.com>
2025-01-14 21:41:47 +0100

    leap: add custom mappings
    to not conflict with mini-surround

--- lua/arctgx/pluginConfigs/leap-nvim.lua
+++ lua/arctgx/pluginConfigs/leap-nvim.lua
@@ -1,1 +1,4 @@
-require('leap').set_default_keymaps()
+vim.keymap.set('n', 'gs', '<Plug>(leap)')
+vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
+vim.keymap.set({'x', 'o'}, 'gs', '<Plug>(leap-forward)')
+vim.keymap.set({'x', 'o'}, 'S', '<Plug>(leap-backward)')
