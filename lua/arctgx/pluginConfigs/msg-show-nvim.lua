67f4df96cfa8bb6f56d6ab687e4d26fc8d8e67ca
przepompownia <przepompownia@users.noreply.github.com>
2025-02-03 00:24:24 +0100

    msg-show-nvim: add clearPromptMessage callback

--- lua/arctgx/pluginConfigs/msg-show-nvim.lua
+++ lua/arctgx/pluginConfigs/msg-show-nvim.lua
@@ -1,5 +1,5 @@
 local notifier = require('msg-show.notifier')
 notifier.setup()
 
-require('msg-show.redir').init(notifier.addUiMessage, notifier.updateUiMessage, notifier.debug)
+require('msg-show.redir').init(notifier.addUiMessage, notifier.updateUiMessage, notifier.debug, notifier.clearPromptMessage)
 require('arctgx.vim.abstractKeymap').set('n', 'browseNotificationHistory', notifier.showHistory)
