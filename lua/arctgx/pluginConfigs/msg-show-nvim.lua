958a714861867e80b57942bd175a670218fb115d
przepompownia <przepompownia@users.noreply.github.com>
2025-02-22 03:13:47 +0100

    Update msg-show-nvim config

--- lua/arctgx/pluginConfigs/msg-show-nvim.lua
+++ lua/arctgx/pluginConfigs/msg-show-nvim.lua
@@ -5,7 +5,6 @@ require('msg-show.redir').init(
   notifier.addUiMessage,
   notifier.updateUiMessage,
   notifier.debug,
-  notifier.showDialogMessage,
-  notifier.clearPromptMessage
+  notifier.showDialogMessage
 )
 require('arctgx.vim.abstractKeymap').set('n', 'browseNotificationHistory', notifier.showHistory)
