c61643da0c0c9f3af963eaa128d842bc0d2668f5
przepompownia <przepompownia@users.noreply.github.com>
2025-02-25 17:18:46 +0100

    lsp: improve delaying documentation on CompleteChanged

--- lua/arctgx/completion.lua
+++ lua/arctgx/completion.lua
@@ -1,46 +1,51 @@
 local api = vim.api
 -- It's experimental and rather still not very usable
 local completion = {}
+--- @type fun
+local cancelResolveCb = nil
+
 
 local debounce = 300
 local useBuiltinAutotrigger = false
-local timer = assert(vim.uv.new_timer())
+local insertCharTimer = assert(vim.uv.new_timer())
+local completeChangedTimer = assert(vim.uv.new_timer())
 local completionAugroup = api.nvim_create_augroup('arctgx.completion', {clear = true})
 
 local function showDocumentation(buf, clientId)
-  local info = vim.fn.complete_info({'selected'})
   local completionItem = vim.tbl_get(vim.v.completed_item, 'user_data', 'nvim', 'lsp', 'completion_item')
   if nil == completionItem then
     return
   end
 
-  local resolvedItem = vim.lsp.buf_request_sync(
+  local info = vim.fn.complete_info({'selected'})
+
+  return vim.lsp.buf_request_all(
     buf,
     vim.lsp.protocol.Methods.completionItem_resolve,
     completionItem,
-    500
+    function (results, _context)
+      for respClientId, resolvedItem in pairs(results) do
+        if respClientId ~= clientId then
+          return
+        end
+
+        local docs = vim.tbl_get(resolvedItem, 'result', 'documentation', 'value')
+        if nil == docs then
+          return
+        end
+
+        local winData = api.nvim__complete_set(info['selected'], {info = docs})
+        if not winData.winid or not api.nvim_win_is_valid(winData.winid) then
+          return
+        end
+
+        api.nvim_win_set_config(winData.winid, {border = 'rounded'})
+
+        vim.treesitter.start(winData.bufnr, 'markdown')
+        vim.wo[winData.winid].conceallevel = 3
+      end
+    end
   )
-
-  if nil == resolvedItem then
-    return
-  end
-
-  -- vim.print(resolvedItem[clientId])
-  local docs = vim.tbl_get(resolvedItem[clientId], 'result', 'documentation', 'value')
-  if nil == docs then
-    return
-  end
-
-  local winData = api.nvim__complete_set(info['selected'], {info = docs})
-  if not winData.winid or not api.nvim_win_is_valid(winData.winid) then
-    return
-  end
-
-  api.nvim_win_set_config(winData.winid, {border = 'rounded'})
-
-  -- vim.lsp.util.convert_input_to_markdown_lines, documentation.kind <> markdown?
-  vim.treesitter.start(winData.bufnr, 'markdown')
-  vim.wo[winData.winid].conceallevel = 3
 end
 
 function completion.hasWordsBefore()
@@ -51,19 +56,28 @@ end
 
 local function autotrigger(triggerCharacters)
   -- https://github.com/neovim/neovim/pull/30028#issuecomment-2285503268
-  if vim.fn.pumvisible() == 1 then
-    return
-  end
-  if timer:get_due_in() > 0 then
-    -- vim.notify('Inactive ' .. tostring(timer:get_due_in()))
+  if vim.fn.pumvisible() == 1 or insertCharTimer:get_due_in() > 0 then
     return
   end
 
   if vim.tbl_contains(triggerCharacters, vim.v.char) or vim.v.char:match('[%w_]') then
-    -- vim.notify('Triggered after ' .. vim.v.char, vim.log.levels.DEBUG)
-    timer:start(debounce, 0, vim.schedule_wrap(vim.lsp.completion.trigger))
+    insertCharTimer:start(debounce, 0, vim.schedule_wrap(vim.lsp.completion.trigger))
   end
-  -- vim.notify('Triggering completion ' .. api.nvim_get_current_line())
+end
+
+local function delayShowDoc(buf, clientId)
+  if vim.fn.pumvisible() ~= 1 then
+    return
+  end
+  if completeChangedTimer:get_due_in() > 0 then
+    completeChangedTimer:again()
+    if type(cancelResolveCb) == 'function' then
+      cancelResolveCb()
+    end
+  end
+  completeChangedTimer:start(debounce, 0, vim.schedule_wrap(function ()
+    cancelResolveCb = showDocumentation(buf, clientId)
+  end))
 end
 
 local keycodes = {
@@ -101,7 +115,7 @@ function completion.init()
         'completionProvider',
         'triggerCharacters'
       ) or {}
-      -- vim.print(triggerCharacters)
+
       vim.lsp.completion.enable(true, clientId, args.buf, {autotrigger = useBuiltinAutotrigger})
 
       vim.keymap.set({'i'}, '<C-Space>', vim.lsp.completion.trigger, {buffer = args.buf})
@@ -123,8 +137,7 @@ function completion.init()
           group = completionAugroup,
           buffer = args.buf,
           callback = function ()
-            -- vim.print(args.data)
-            showDocumentation(args.buf, clientId)
+            delayShowDoc(args.buf, clientId)
           end,
         })
       end
@@ -166,29 +179,6 @@ function completion.init()
       end,
       {expr = true, desc = ('%s to %s if pumvisible'):format(insertKmap, pumKmap)}
     )
-  end
-
-  local tabMaps = {
-    ['<Tab>'] = {pum = '<C-n>', snippetJump = 1},
-    ['<S-Tab>'] = {pum = '<C-p>', snippetJump = -1},
-  }
-
-  for key, params in pairs(tabMaps) do
-    vim.keymap.set({'i'}, key, function ()
-      if vim.fn.pumvisible() == 1 then
-        return params.pum
-      elseif vim.snippet.active({direction = params.snippetJump}) then
-        vim.snippet.jump(params.snippetJump)
-        return
-      elseif completion.hasWordsBefore() then
-        vim.lsp.completion.trigger()
-      else
-        return key
-      end
-    end, {
-      expr = true,
-      silent = true,
-    })
   end
 
   vim.keymap.set({'i'}, '<CR>', function ()
