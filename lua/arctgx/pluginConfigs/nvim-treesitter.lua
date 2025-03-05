96877426ff3576dda2b42eabdb4714e799007f28
przepompownia <przepompownia@users.noreply.github.com>
2025-03-05 21:01:45 +0100

    Add `TSUpdateSync` command

--- lua/arctgx/pluginConfigs/nvim-treesitter.lua
+++ lua/arctgx/pluginConfigs/nvim-treesitter.lua
@@ -9,3 +9,25 @@ require('nvim-treesitter.install').install(
     vim.api.nvim_exec_autocmds('User', {pattern = 'TSInstallFinished'})
   end
 )
+
+vim.api.nvim_create_user_command('TSUpdateSync', function (args)
+  local okv = 'something never passed to cb'
+
+  local ok = okv
+
+  require('nvim-treesitter.install').update(
+    nil,
+    nil,
+    function (success)
+      ok = success
+    end
+  )
+
+  vim.wait(15000, function ()
+    return ok ~= okv
+  end)
+
+  if args.args == 'quit' then
+    vim.cmd.cquit({count = (ok ~= false) and 0 or 1})
+  end
+end, {nargs = '?'})
