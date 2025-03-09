e186f882d3bcca4f320ec3e21e602d41be54cd3f
przepompownia <przepompownia@users.noreply.github.com>
2025-03-09 21:01:07 +0100

    msg-show: follow changes

--- lua/arctgx/pluginConfigs/msg-show-nvim.lua
+++ lua/arctgx/pluginConfigs/msg-show-nvim.lua
@@ -1,6 +1,5 @@
-local notifier = require('msg-show.notifier')
-notifier.setup()
+local msgShow = require('msg-show')
 
-require('msg-show').init()
-require('arctgx.vim.abstractKeymap').set('n', 'notificationBrowseHistory', notifier.showHistory)
-require('arctgx.vim.abstractKeymap').set('n', 'notificationDelayRemoval', notifier.delayRemoval)
+msgShow.setup()
+require('arctgx.vim.abstractKeymap').set('n', 'notificationBrowseHistory', msgShow.history)
+require('arctgx.vim.abstractKeymap').set('n', 'notificationDelayRemoval', msgShow.delayRemoval)
