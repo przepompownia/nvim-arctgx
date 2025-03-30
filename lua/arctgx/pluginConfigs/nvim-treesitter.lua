a5975f726def9412c360f28c8ad58d5a833f1169
przepompownia <przepompownia@users.noreply.github.com>
2025-03-30 20:03:19 +0200

    nvim-treesitter: follow changes how to select uninstalled languages

--- lua/arctgx/pluginConfigs/nvim-treesitter.lua
+++ lua/arctgx/pluginConfigs/nvim-treesitter.lua
@@ -1,10 +1,8 @@
-local langs = require('arctgx.treesitter').langs()
-
 require('nvim-treesitter').setup({})
 
 require('nvim-treesitter.install').install(
-  langs,
-  {skip = {installed = true}},
+  require('nvim-treesitter.config').norm_languages(require('arctgx.treesitter').langs(), {installed = true}),
+  {},
   function ()
     vim.api.nvim_exec_autocmds('User', {pattern = 'TSInstallFinished'})
   end
