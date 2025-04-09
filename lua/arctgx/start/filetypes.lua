db1142df2bcabd9fed5c5494e7888edec5c0c0ae
przepompownia <przepompownia@users.noreply.github.com>
2025-04-09 17:44:36 +0200

    FileType changes

--- lua/arctgx/start/filetypes.lua
+++ lua/arctgx/start/filetypes.lua
@@ -18,3 +18,19 @@ api.nvim_create_autocmd({'FileType'}, {
     vim.cmd([[silent wincmd T]])
   end
 })
+
+api.nvim_create_autocmd({'FileType'}, {
+  pattern = {'sql', 'mysql'},
+  group = augroup,
+  nested = true,
+  callback = function (ev)
+    vim.opt_local.isfname:append('`')
+    vim.bo.softtabstop = 2
+    vim.bo.shiftwidth = 2
+    vim.bo.expandtab = true
+    vim.g.loaded_sql_completion = 1
+    vim.g.omni_sql_no_default_maps = 1
+    vim.g.ftplugin_sql_omni_key = '<Nop>'
+    vim.keymap.set({'n', 'i', 'v'}, '<F2>', vim.cmd.write, {buffer = ev.buf, desc = 'Write SQL query'})
+  end
+})
