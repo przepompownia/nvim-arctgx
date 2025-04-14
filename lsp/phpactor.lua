1a05c75fd7eee85fa1ae08f84f0401558a16fd69
przepompownia <przepompownia@users.noreply.github.com>
2025-04-15 01:10:36 +0200

    Reduce phpactor related configuration to single file

--- lsp/phpactor.lua
+++ lsp/phpactor.lua
@@ -1,3 +1,25 @@
+local api = vim.api
+
+local function dumpConfig()
+  vim.lsp.buf_request_all(0, 'phpactor/debug/config', {['return'] = true}, function (result)
+    for _, data in pairs(result) do
+      require('arctgx.base').displayInWindow('Phpactor LSP Configuration', 'json', data.result)
+    end
+  end)
+end
+
+local function status()
+  vim.lsp.buf_request_all(0, 'phpactor/status', {['return'] = true}, function (result)
+    for _, data in pairs(result) do
+      require('arctgx.base').displayInWindow('Phpactor Status', 'markdown', data.result)
+    end
+  end)
+end
+
+local function reindex()
+  vim.lsp.buf_notify(0, 'phpactor/indexer/reindex', {})
+end
+
 return {
   cmd = {
     -- 'phpxx',
@@ -19,4 +41,9 @@ return {
     ['language_server_worse_reflection.inlay_hints.params'] = true,
     -- ['indexer.searcher_semi_fuzzy'] = true,
   },
+  on_attach = function (_, bufnr)
+    api.nvim_buf_create_user_command(bufnr, 'PhpactorLSPDumpConfig', dumpConfig, {nargs = 0})
+    api.nvim_buf_create_user_command(bufnr, 'PhpactorLSPServerStatus', status, {nargs = 0})
+    api.nvim_buf_create_user_command(bufnr, 'PhpactorLSPServerReindex', reindex, {nargs = 0})
+  end
 }
