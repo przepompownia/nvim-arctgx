2f9ac21b1f31057b8f9349b8b0e74ea4d94597f5
przepompownia <przepompownia@users.noreply.github.com>
2025-05-11 02:26:33 +0200

    Configure dap-view

---
+++ lua/arctgx/pluginConfigs/nvim-dap-view.lua
@@ -0,0 +1,82 @@
+local api = vim.api
+local keymap = require('arctgx.vim.abstractKeymap')
+
+require('arctgx.lazy').setupOnLoad('dap', function ()
+  local dv = require('dap-view')
+  local dap = require('dap')
+  local augroup = api.nvim_create_augroup('arctgx.dap-view', {clear = true})
+
+  dap.defaults.fallback.switchbuf = 'useopen'
+  dv.setup({
+    windows = {
+      terminal = {
+        hide = {'php'},
+      },
+    },
+  })
+  dap.listeners.before.event_stopped['dap-view-config'] = function ()
+    api.nvim_exec_autocmds('FileType', {
+      group = augroup,
+      pattern = vim.bo.ft,
+    })
+    dv.open()
+  end
+  dap.listeners.before.event_terminated['dap-view-config'] = function ()
+    dv.close()
+  end
+  dap.listeners.before.event_exited['dap-view-config'] = function ()
+    dv.close()
+  end
+
+  api.nvim_create_autocmd('User', {
+    group = augroup,
+    pattern = 'DAPClean',
+    callback = function () dv.close() end,
+  })
+
+  local filetypes = vim.tbl_keys(require('arctgx.dap').getDeclaredConfigurations())
+  filetypes[#filetypes + 1] = 'dap-view'
+
+  api.nvim_create_autocmd({'FileType'}, {
+    pattern = filetypes,
+    group = augroup,
+    callback = function (event)
+      keymap.set(
+        {'n'},
+        'debuggerOpenViewScopes',
+        function () dv.jump_to_view('scopes') end,
+        {buffer = event.buf}
+      )
+      keymap.set(
+        {'n'},
+        'debuggerOpenViewBreakpoints',
+        function () dv.jump_to_view('breakpoints') end,
+        {buffer = event.buf}
+      )
+      keymap.set(
+        {'n'},
+        'debuggerOpenViewExceptions',
+        function () dv.jump_to_view('exceptions') end,
+        {buffer = event.buf}
+      )
+      keymap.set(
+        {'n'},
+        'debuggerOpenViewThreads',
+        function () dv.jump_to_view('threads') end,
+        {buffer = event.buf}
+      )
+      keymap.set(
+        {'n'},
+        'debuggerOpenViewWatches',
+        function () dv.jump_to_view('watches') end,
+        {buffer = event.buf}
+      )
+      keymap.set(
+        {'n'},
+        'debuggerOpenViewREPL',
+        function () dv.jump_to_view('repl') end,
+        {buffer = event.buf}
+      )
+    end
+  })
+end)
