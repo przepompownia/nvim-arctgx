6790695a940e0f052df1a881de1b26a8a85b8cbf
przepompownia <przepompownia@users.noreply.github.com>
2025-09-17 23:59:50 +0200

    Add initial autocomple module

---
+++ lua/arctgx/autocomplete.lua
@@ -0,0 +1,20 @@
+local api = vim.api
+local augroup = api.nvim_create_augroup('arctgx.autocomplete', {clear = true})
+
+local autocomplete = {}
+
+function autocomplete.enable()
+  vim.o.autocomplete = true
+  api.nvim_create_autocmd('OptionSet', {
+    group = augroup,
+    pattern = 'buftype',
+    callback = function ()
+      if vim.v.option_new ~= 'prompt' then
+        return
+      end
+      vim.bo[api.nvim_get_current_buf()].complete = ''
+    end
+  })
+end
+
+return autocomplete
