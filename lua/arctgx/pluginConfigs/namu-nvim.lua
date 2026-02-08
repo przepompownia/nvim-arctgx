604a1a4f2cffa2c25c32d566fb461ce7c4e1837d
przepompownia <przepompownia@users.noreply.github.com>
2026-02-08 12:51:35 +0100

    namu: lazy loading

--- lua/arctgx/pluginConfigs/namu-nvim.lua
+++ lua/arctgx/pluginConfigs/namu-nvim.lua
@@ -1,10 +1,12 @@
-require('namu').setup({
-  namu_symbols = {
-    options = {
-      row_position = 'bottom',
+require('arctgx.lazy').setupOnLoad('namu', function ()
+  require('namu').setup({
+    namu_symbols = {
+      options = {
+        row_position = 'bottom',
+      },
     },
-  },
-})
+  })
+end)
 
 require('arctgx.vim.abstractKeymap').set({'n'}, 'langFindDocumentSymbols', function ()
   require('namu.namu_symbols').show()
