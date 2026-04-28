2fc03d53b9f8ae012373244f43fc4bfca4939592
przepompownia <przepompownia@users.noreply.github.com>
2026-04-28 04:02:26 +0200

    Follow (unstable) diffview highlights

--- lua/arctgx/pluginConfigs/nord-nvim.lua
+++ lua/arctgx/pluginConfigs/nord-nvim.lua
@@ -1,7 +1,8 @@
 vim.g.colorschemeDark = 'nord'
 require('nord').setup {
   diff = {mode = 'fg'},
-  on_highlights = function (highlights, _colors)
+  on_highlights = function (highlights, colors)
     highlights['@error'] = {fg = '#880000'}
+    highlights['DiffviewCommitLocalOnly'] = {fg = colors.aurora.yellow}
   end,
 }
