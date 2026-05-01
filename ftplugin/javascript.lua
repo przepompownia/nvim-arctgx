5bab004b5e50943c3c81608832fa079786fae519
przepompownia <przepompownia@users.noreply.github.com>
2026-05-01 13:53:32 +0200

    JS: add imaps

--- ftplugin/javascript.lua
+++ ftplugin/javascript.lua
@@ -2,3 +2,6 @@ vim.bo.tabstop = 2
 vim.bo.softtabstop = 2
 vim.bo.shiftwidth = 2
 vim.bo.expandtab = true
+vim.keymap.set('i', '<M-S-.>', '=>', {buffer = true})
+vim.keymap.set('i', '<M-S->>', '=>', {buffer = true})
+vim.keymap.set('i', '<A-char-62>', '=>', {buffer = true})
