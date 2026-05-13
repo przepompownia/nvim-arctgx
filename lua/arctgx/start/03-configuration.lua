afc31077d1a021cb7b9e411729e006b9b571d791
przepompownia <przepompownia@users.noreply.github.com>
2026-05-13 12:38:45 +0200

    Move pluginConfigs/arctgx to 03-configuration

--- lua/arctgx/start/03-configuration.lua
+++ lua/arctgx/start/03-configuration.lua
@@ -5,6 +5,16 @@ local pluginPrefix = 'arctgx.pluginConfigs'
 
 plugin.loadCustomConfiguration(vim.g.pluginDirs or {}, pluginPrefix)
 
+local keymap = require('arctgx.vim.abstractKeymap')
+vim.g.arctgxBackupDir = vim.fs.joinpath(vim.env['XDG_CONFIG_HOME'] or os.getenv('HOME'), '.config', 'backups')
+keymap.set('n', 'writeBackup', function ()
+  require('arctgx.writebackup').save()
+end)
+
+keymap.set('n', 'langIncrementSelection', function () require('arctgx.treesitter.incremental-selection').selectNodeUnderCursor() end)
+keymap.set('v', 'langIncrementSelection', function () require('arctgx.treesitter.incremental-selection').incrementSelection() end)
+keymap.set('v', 'langDecrementSelection', function () require('arctgx.treesitter.incremental-selection').decrementSelection() end)
+
 api.nvim_create_autocmd('VimEnter', {
   once = true,
   pattern = '*',
