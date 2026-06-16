559a08fbbb283c4118c63406451f3455ec986f3e
przepompownia <przepompownia@users.noreply.github.com>
2026-06-17 00:28:18 +0200

    Remove `Mks` - use `SessionWritePre`

--- lua/arctgx/session.lua
+++ lua/arctgx/session.lua
@@ -1,43 +1,16 @@
+local api = vim.api
+
 local extension = {}
-
---- @todo hook names
-local hooks = {
-  save = {
-    ---@type table<{name: string, callback: function}>
-    before = {},
-  },
-}
-
-local function appendHook(name, hook, event, when)
-  if type(hook) ~= 'function' then
-    vim.notify(
-      ('Cannot add hook (%s.%s). It must be a function.'):format(event, when),
-      vim.log.levels.ERROR
-    )
-    return
-  end
-
-  hooks[event][when][#hooks[event][when] + 1] = {name = name, callback = hook}
-end
-
-local function runHooks(event, when)
-  for _, hook in ipairs(hooks[event][when]) do
-    vim.notify(('Running "%s %s" hook "%s".'):format(when, event, hook.name), vim.log.levels.DEBUG)
-    if false == hook.callback() then
-      vim.notify(('Cannot run "%s %s" hook "%s".'):format(when, event, hook.name), vim.log.levels.WARN)
-
-      return false
-    end
-  end
-end
 
 --- @param hook function
 function extension.appendBeforeSaveHook(name, hook)
-  appendHook(name, hook, 'save', 'before')
-end
-
-function extension.runBeforeSaveHooks()
-  return runHooks('save', 'before')
+  api.nvim_create_autocmd('SessionWritePre', {
+    group = api.nvim_create_augroup('arctgx.session.pre', {clear = false}),
+    callback = function (ev)
+      vim.notify(('Running hook "%s".'):format(name), vim.log.levels.DEBUG)
+      hook(ev)
+    end
+  })
 end
 
 return extension
