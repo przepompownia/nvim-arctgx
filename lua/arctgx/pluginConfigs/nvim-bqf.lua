5e4a78318c6524c20102e123f94fddfa2e7b955f
przepompownia <przepompownia@users.noreply.github.com>
2026-09-06 01:30:02 +0200

    nvim-bqf: fix lazy packadd

--- lua/arctgx/pluginConfigs/nvim-bqf.lua
+++ lua/arctgx/pluginConfigs/nvim-bqf.lua
@@ -1,33 +1,48 @@
 require('arctgx.lazy').setupOnLoad('bqf', {
   before = function () vim.cmd.packadd('nvim-bqf') end,
-})
-require('bqf').setup({
-  auto_enable = true,
-  preview = {
-    win_height = 12,
-    win_vheight = 12,
-    delay_syntax = 80,
-    border_chars = {'┃', '┃', '━', '━', '┏', '┓', '┗', '┛', '█'},
-    should_preview_cb = function (bufnr)
-      local ret = true
-      local filename = vim.api.nvim_buf_get_name(bufnr)
-      local fsize = vim.fn.getfsize(filename)
+  after = function ()
+    require('bqf').setup({
+      auto_enable = true,
+      preview = {
+        win_height = 12,
+        win_vheight = 12,
+        delay_syntax = 80,
+        border_chars = {'┃', '┃', '━', '━', '┏', '┓', '┗', '┛', '█'},
+        should_preview_cb = function (bufnr)
+          local ret = true
+          local filename = vim.api.nvim_buf_get_name(bufnr)
+          local fsize = vim.fn.getfsize(filename)
 
-      if vim.startswith(filename, 'fugitive://') then
-        return false
-      end
-      -- file size greater than 100k can't be previewed automatically
-      if fsize > 100 * 1024 then
-        ret = false
-      end
-      return ret
+          if vim.startswith(filename, 'fugitive://') then
+            return false
+          end
+          -- file size greater than 100k can't be previewed automatically
+          if fsize > 100 * 1024 then
+            ret = false
+          end
+          return ret
+        end
+      },
+      func_map = {
+        openc = '<CR>',
+        open = '',
+        vsplit = '',
+        ptogglemode = ',p',
+        stoggleup = '',
+      },
+    })
+  end
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  callback = function (ev)
+    if ev.match ~= 'qf' then
+      return
     end
-  },
-  func_map = {
-    openc = '<CR>',
-    open = '',
-    vsplit = '',
-    ptogglemode = ',p',
-    stoggleup = '',
-  },
+
+    require('bqf')
+    require('bqf.main').enable()
+
+    return true
+  end
 })
