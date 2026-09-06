ba9c86c4d554eb2a948181fd0bc44dbd3526c9df
przepompownia <przepompownia@users.noreply.github.com>
2026-09-06 21:48:20 +0200

    Lazy load actions-preview-nvim

--- lua/arctgx/pluginConfigs/actions-preview-nvim.lua
+++ lua/arctgx/pluginConfigs/actions-preview-nvim.lua
@@ -1,16 +1,29 @@
-require('actions-preview').setup {
-  telescope = {
-    sorting_strategy = 'ascending',
-    layout_strategy = 'vertical',
-    layout_config = {
-      width = 0.8,
-      height = 0.9,
-      prompt_position = 'top',
-      preview_cutoff = 20,
-      preview_height = function (_, _, max_lines)
-        return max_lines - 15
-      end,
-    },
-  },
-}
-vim.lsp.buf.code_action = require('actions-preview').code_actions
+require('arctgx.lazy').setupOnLoad('actions-preview', {
+  before = function ()
+    vim.cmd.packadd('actions-preview.nvim')
+  end,
+  after = function ()
+    require('actions-preview').setup {
+      telescope = {
+        sorting_strategy = 'ascending',
+        layout_strategy = 'vertical',
+        layout_config = {
+          width = 0.8,
+          height = 0.9,
+          prompt_position = 'top',
+          preview_cutoff = 20,
+          preview_height = function (_, _, max_lines)
+            return max_lines - 15
+          end,
+        },
+      },
+      -- highlight_command = {
+      -- require('actions-preview.highlight').delta(),
+      -- require('actions-preview.highlight').diff_highlight(),
+      -- },
+    }
+  end
+})
+vim.lsp.buf.code_action = function ()
+  require('actions-preview').code_actions()
+end
